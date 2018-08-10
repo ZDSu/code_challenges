0b606f96a49627f35298ba043e62e2caee2ba997
Beverly Pham <beverly.pham@gmail.com>
2018-08-10 13:17:22 -0700

    longest common prefix solved

---
+++ leetcode/long_common_prefix.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/longest-common-prefix/
+# https://leetcode.com/problems/longest-common-prefix/solution/
+
+
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 0 or strs[0] == '':
+            return ''
+        prefix = ''
+        for i in range(len(strs[0])):
+            prefix += strs[0][i]
+            for each in strs:
+                if each.startswith(prefix):
+                    continue
+                else:
+                    return prefix[:-1]
+        return prefix
