244f69d507b7a1ca3aeb7795fe0f4aa2bf208a61
Beverly Pham <beverly.pham@gmail.com>
2018-08-15 13:10:02 -0700

    same tree solved with notes on how to do recursively in 1 line

--- leetcode/same_bt.py
+++ leetcode/same_bt.py
@@ -7,6 +7,13 @@
 #         self.left = None
 #         self.right = None
 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution(object):
     def isSameTree(self, p, q):
         """
@@ -14,26 +21,41 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
-        if not p or not q:
+        if (p and not q) or (q and not p):
             return False
+        if not p and not q:
+            return True
         
         queue1 = [p]
         queue2 = [q]
         
         while queue1:
-            if not queue2:
-                return False
             curr1 = queue1.pop(0)
             curr2 = queue2.pop(0)
             if curr1.val != curr2.val:
                 return False
-            if curr1.left:
+            if curr1.left and curr2.left:
                 queue1.append(curr1.left)
-            if curr1.right:
-                queue1.append(curr1.right)
-            if curr2.left:
                 queue2.append(curr2.left)
-            if curr2.right:
+            elif (curr1.left and not curr2.left) or (curr2.left and not curr1.left):
+                return False
+            if curr1.right and curr2.right:
+                queue1.append(curr1.right)
                 queue2.append(curr2.right)
+            elif (curr1.right and not curr2.right) or (curr2.right and not curr1.right):
+                return False                
         return True
-            
+
+
+# test cases: 
+# [] [] 
+# [1,2] [1, null, 2]
+
+# How to think about this for a 1-line solution:
+# The example submission for the 100% C solution is about 30 lines. Yikes. Think more, code less. Only one line is needed.
+# If tree p is empty, then return true if and only if q is empty. Otherwise p is not empty, so return true if and only if q is also not empty, its root value matches the root value of q, and both left and right subtrees match.
+# Now both of these alternatives return, so we can put both branches in a ternary expression and return its value instead:
+
+# bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+#   return !p ? !q : q && p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+# }
