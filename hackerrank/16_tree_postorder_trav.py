f73975f1fb1aac08254a64f9f992b43df12b9fa1
Beverly Pham <beverly.pham@gmail.com>
2018-08-16 01:18:17 -0700

    postorder traversal of BT solved

---
+++ hackerrank/16_tree_postorder_trav.py
@@ -0,0 +1,29 @@
+# https://www.hackerrank.com/challenges/tree-postorder-traversal/problem
+
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def postOrder(root):
+    #Write your code here
+    traverse = ''
+    
+    def _walk(node=None):
+        nonlocal traverse
+        if node is None:
+            return
+        
+        if node.left:
+            _walk(node.left)
+            
+        if node.right:
+            _walk(node.right)
+            
+        traverse += str(node.info) + ' '
+    
+    _walk(root)
+    
+    return print(traverse[:-1])
