03330404548b36972aaf945ac5c0a4ae9f5ef543
Beverly Pham <beverly.pham@gmail.com>
2018-08-16 01:14:52 -0700

    preorder traversal of BT solved

---
+++ hackerrank/15_tree_preorder_trav.py
@@ -0,0 +1,29 @@
+# https://www.hackerrank.com/challenges/tree-preorder-traversal/problem
+
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def preOrder(root):
+    #Write your code here
+    traverse = ''
+    
+    def _walk(node=None):
+        nonlocal traverse
+        if node is None:
+            return
+        
+        traverse += str(node.info) + ' '
+        
+        if node.left:
+            _walk(node.left)
+        
+        if node.right:
+            _walk(node.right)
+
+    _walk(root)
+    
+    return print(traverse[:-1])
