102ba01a9ffe227fa0e77bc8dd7922ae4b3c3205
Beverly Pham <beverly.pham@gmail.com>
2018-08-16 00:12:31 -0700

    valid palindrome solved iteratively, better solution

--- leetcode/valid_palindrome.py
+++ leetcode/valid_palindrome.py
@@ -9,3 +9,23 @@ class Solution(object):
         """
         s = re.sub('[^A-Za-z0-9]+', '', s).lower()
         return s == s[::-1]
+
+
+# iteratively, better solution
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = re.sub('[^A-Za-z0-9]+', '', s).lower()
+        
+        i = 0
+        j = len(s) - 1
+        
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
