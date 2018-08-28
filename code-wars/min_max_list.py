a8a172f57e2ce8998f567aeac9b9e37dd3c2c521
Beverly Pham <beverly.pham@gmail.com>
2018-08-28 01:27:04 -0700

    min/max of list solved

--- code-wars/min_max_list.py
+++ code-wars/min_max_list.py
@@ -6,3 +6,27 @@ def minimum(arr):
 
 def maximum(arr):
     return max(arr)
+
+
+# another solution
+def min(arr):
+    return sorted(arr)[0]
+
+def max(arr):
+    return sorted(arr)[-1]
+
+
+# not using built-in methods
+def min(arr):
+    low = arr[0]
+    for i in arr[1:]:
+        if i < low:
+            low = i
+    return low
+
+def max(arr):
+    high = arr[0]
+    for i in arr[1:]:
+        if i > high:
+            high = i
+    return high
