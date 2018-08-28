fc53828437df86f5eabe9c315864bbbfaa37190c
Beverly Pham <beverly.pham@gmail.com>
2018-08-28 01:29:11 -0700

    even or odd solved

---
+++ code-wars/even_or_odd.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/
+
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        return 'Even'
+    return 'Odd'
+
+
+# two ways using ternary operator
+def even_or_odd(number):
+  return 'Odd' if number % 2 else 'Even'
+
+
+def even_or_odd(number):
+  return 'Even' if number % 2 == 0 else 'Odd'
