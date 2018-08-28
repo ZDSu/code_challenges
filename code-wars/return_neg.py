5f77f0b73fd8b8a8c9086c057e06cbe55a942967
Beverly Pham <beverly.pham@gmail.com>
2018-08-28 01:15:42 -0700

    return negative solved

---
+++ code-wars/return_neg.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/55685cd7ad70877c23000102/
+
+
+def make_negative( number ):
+    if number < 0:
+        return number
+    return number * -1
+
+# or simply
+def make_negative( number ):
+    return -abs(number)
