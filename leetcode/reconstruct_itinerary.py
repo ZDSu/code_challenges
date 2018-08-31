0aaa17253d1b73f1af5aba65c9e28943af983a30
Beverly Pham <beverly.pham@gmail.com>
2018-08-30 22:47:40 -0700

    working better but still not perfect

--- leetcode/reconstruct_itinerary.py
+++ leetcode/reconstruct_itinerary.py
@@ -6,22 +6,26 @@ class Solution(object):
         """
         :type tickets: List[List[str]]
         :rtype: List[str]
-        """
+        """    
         itinerary = ['JFK']
         flights = {}
         start = 'JFK'
             
         for dep, arr in tickets:
             if dep not in flights:
-                flights[dep] = arr
+                flights[dep] = [arr]
             else:
-                flights[dep].extend(arr)
+                flights[dep].append(arr)
+                flights[dep] = sorted(flights[dep])
         
         for i in range(len(tickets)):
-            if start:
-                itinerary.append(flights[start])
-                start = flights[start]
-                # start = sorted(flights[first])[0]
+            start = flights[start].pop(0)
+            itinerary.append(start)
         return itinerary
-            
-                
+
+
+# test cases:
+# [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]  gives Line 19: KeyError: 'KUL'
+# findItinerary([["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"],["SJC","JFK"],["JFK","ZBC"]])
+
+# [["JFK","KUL"],["JFK","NRT"],["NRT","JFK"]] should return ["JFK","NRT","JFK","KUL"]
