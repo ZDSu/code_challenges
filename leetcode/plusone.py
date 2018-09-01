9c0ab4b981a9ff725e9d98ec287be1966a033483
Beverly Pham <beverly.pham@gmail.com>
2018-08-31 17:21:26 -0700

    another solution added

--- leetcode/plusone.py
+++ leetcode/plusone.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/plus-one/description/
 
 
+# (44 ms)
 class Solution:
     def plusOne(self, digits):
         """
@@ -15,3 +16,19 @@ class Solution:
         for each in number:
             plus.append(int(each))
         return plus
+
+
+# using recursion (48 ms)
+class Solution:
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        if digits[-1] == 9:
+            if len(digits) == 1:
+                return [1, 0]
+            return self.plusOne(digits[:-1]) + [0]
+        else:
+            digits[-1] += 1
+        return digits
