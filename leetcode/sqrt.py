ab935fe3c59cf181e372bef47bae6e2012362462
Beverly Pham <beverly.pham@gmail.com>
2018-08-31 18:48:38 -0700

    square root solved but try to do again using binary search

---
+++ leetcode/sqrt.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/sqrtx/description/
+
+
+class Solution:
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(math.sqrt(x))      
