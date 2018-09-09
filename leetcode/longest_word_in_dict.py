32a86d780e004d6fde386eba7e33196c4bfa77c8
Beverly Pham <beverly.pham@gmail.com>
2018-09-08 21:12:46 -0700

    longest word in dict passes 43/57 cases, need to refactor to work for different words

---
+++ leetcode/longest_word_in_dict.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/longest-word-in-dictionary/description/
+
+
+class Solution:
+    def longestWord(self, words):
+        """
+        :type words: List[str]
+        :rtype: str
+        """
+        result = ''
+        for word in words:
+            if word[:-1] in words:
+                if len(word) > len(result):
+                    result = word
+                elif len(word) == len(result):
+                    result = sorted([word, result])[0]
+        return result
+
+
+# test cases
+# ["yo","ew","fc","zrc","yodn","fcm","qm","qmo","fcmz","z","ewq","yod","ewqz","y"] returns "yodn"
