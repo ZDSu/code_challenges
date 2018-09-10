ce5ea3602a811540abe266d07bbc12d2568233c5
Beverly Pham <beverly.pham@gmail.com>
2018-09-09 22:45:21 -0700

    contains duplicate solved

---
+++ leetcode/contains_duplicate.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/contains-duplicate/description/
+# https://leetcode.com/articles/contains-duplicate/
+
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        numbers = set()
+        for each in nums:
+            if each in numbers:
+                return True
+            numbers.add(each)
+        return False
+
+
+# one line
+# class Solution(object):
+#     def containsDuplicate(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: bool
+#         """
+#         return len(set(nums)) < len(nums)
