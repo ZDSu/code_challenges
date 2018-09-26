50c7eba13d09f22532cf00597164889db6a9e17a
Beverly Pham <beverly.pham@gmail.com>
2018-09-26 15:45:27 -0700

    add another dynamic solution

--- leetcode/fizzbuzz.py
+++ leetcode/fizzbuzz.py
@@ -19,3 +19,24 @@ class Solution:
             else:
                 result.append(str(num))
         return result
+
+
+# solution using dict, making it more dynamic in case add more conditions
+class Solution:
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result = []
+        map = {3: 'Fizz', 5: 'Buzz'}
+        
+        for num in range(1, n + 1):
+            string = ''
+            for key in map.keys():
+                if num % key == 0:
+                    string += map[key]
+            if not string:
+                string = str(num)
+            result.append(string)
+        return result
