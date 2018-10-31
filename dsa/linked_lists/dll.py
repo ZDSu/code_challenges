b0d466cfc39561171bd6062d62aed9c3962e1f6f
Beverly Pham <beverly.pham@gmail.com>
2018-10-31 16:38:25 -0700

    linked list classes written

--- dsa/linked_lists/dll.py
+++ dsa/linked_lists/dll.py
@@ -1,5 +1,65 @@
-class Node(object):
-    pass
+# Note: This code has not been tested
 
 
-# Create a Doubly Linked List here
+class Node():
+    def __init__(self, value=None):
+        self.val = value
+        self.next = None
+        self.prev = None
+
+
+class DLL():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert(self, value):
+        """Insert at head of list."""
+        node = Node(value)
+        if not self.head:
+            self.head = self.tail = node
+        else:
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
+
+    def append(self, value):
+        """Insert at tail of list."""
+        node = Node(value)
+        if not self.head:
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+
+    def pop(self):
+        """Remove node at head of list."""
+        if not self.head:
+            raise IndexError('List is empty')
+        removed_node = self.head
+        self.head = removed_node.next
+        self.head.prev = None
+        return removed_node
+
+    def remove(self, value):
+        """Remove node with given value."""
+        if not self.head:
+            raise IndexError('List is empty')
+
+        if self.head.val == value:
+            return self.pop()
+
+        curr = self.head
+        prev = None
+        while curr:
+            if curr.val == value:
+                prev.next = curr.next
+                removed_node = curr
+                if curr.next:
+                    curr.next.prev = prev
+                return removed_node
+            prev = curr
+            curr = curr.next
+        
+        raise ValueError('Value not in list')
--- dsa/linked_lists/sll.py
+++ dsa/linked_lists/sll.py
@@ -1,5 +1,65 @@
-class Node(object):
-    pass
+class Node():
+    def __init__(self, value=None):
+        self.val = value
+        self.next = None
 
 
-# Create a Singly Linked List here
+class SLL():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __repr__(self):
+        return f'Head of list is ${self.head.val}'
+
+    def __str__(self):
+        return f'Head of list is ${self.head.val}'
+
+    def insert(self, value):
+        """Insert at head."""
+        node = Node(value)
+        if self.head:
+            node.next = self.head
+        self.head = node
+
+    def append(self, value):
+        """Append to tail."""
+        node = Node(value)
+        if not self.head:
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+
+    def pop(self):
+        """Remove at head."""
+        if not self.head:
+            raise IndexError('List is empty')
+        removed_node = self.head
+        self.head = self.head.next
+        return removed_node.val
+
+    def remove(self, value):
+        """Remove node with given value."""
+        if not self.head:
+            raise IndexError('List is empty')
+        
+        if self.head.val = value:
+            removed_node = self.head
+            self.head = self.head.next
+            return removed_node
+            
+        curr = self.head
+        prev = None
+        while curr:
+            if curr.val == value:
+                if prev:
+                    prev.next = curr.next
+                else:
+                    self.head = curr.next
+                removed_node = curr
+                return curr.val
+                
+            prev = curr
+            curr = curr.next
+        raise ValueError('Value not in list')
