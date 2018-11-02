9ce22f3d6d0fe38b521e889143450e2ec681b6ea
Beverly Pham <beverly.pham@gmail.com>
2018-11-01 22:12:58 -0700

    add another solution

--- misc/arrays8.py
+++ misc/arrays8.py
@@ -1,3 +1,4 @@
+# jay's solutions
 # given an array of 1 to n-1 with one dup, find the dup
 
 def find_dup(arr, n):
@@ -10,9 +11,28 @@ def find_dup(arr, n):
             extraElement = arr[i]
             break
       
-        arr[abs(arr[i])] =- arr[abs(arr[i])]
+        arr[abs(arr[i])] = -arr[abs(arr[i])]
     return abs(extraElement)
       
 arr = [9, 8, 2, 6, 1, 8, 5, 3, 4, 7]
 n = len(arr)
 find_dup(arr, n)
+
+
+# find the only  
+# repeating element in an array  
+# where elements are from 1 to n-1.
+def find_dup(arr, n):
+
+    for i in range(0, n):
+        element = arr[abs(arr[i])]
+
+        if element < 0:
+            return arr[i]
+
+        arr[abs(arr[i])] = -arr[abs(arr[i])]
+
+arr = [9, 8, 2, 6, 1, 8, 5, 3, 4, 7]
+n = len(arr)
+find_dup(arr, n)
+# Big O Time: 0(n)  Space 0(1) becuase of jump list
