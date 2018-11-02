fb101c56ac6ca79c306baaeebece4db7e1ef2a44
Beverly Pham <beverly.pham@gmail.com>
2018-11-01 20:26:56 -0700

    add more performant solution

--- leetcode/maxsubarray.py
+++ leetcode/maxsubarray.py
@@ -13,3 +13,28 @@ class Solution(object):
             curr = max(nums[i], curr + nums[i])
             largest = max(largest, curr)
         return largest
+
+
+# test cases [-2,1]
+
+# beats 98.75%, takes less time to run by not using max function
+class Solution:
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+
+        largest = nums[0]
+        curr = 0
+
+        for num in nums:
+            if curr + num < num:
+                curr = num
+            else:
+                curr += num
+            if largest < curr:
+                largest = curr
+        return largest
