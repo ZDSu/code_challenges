10449370d10e0c8dda5d5e88bd42ad8699678af3
Beverly Pham <beverly.pham@gmail.com>
2018-12-03 22:50:58 -0800

    another solution added

--- leetcode/buy_sell_stock_ii.py
+++ leetcode/buy_sell_stock_ii.py
@@ -28,3 +28,20 @@ class Solution:
         if high:
             profit += high - low
         return profit
+
+
+# based on solution 3  (same runtime as above solution)
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+        
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i-1]
+        return profit
