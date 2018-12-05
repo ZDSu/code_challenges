4900ced14f9e75f486bb2d292179d280be63c743
Beverly Pham <beverly.pham@gmail.com>
2018-12-05 12:26:27 -0800

    two sum solved in a better way

--- leetcode/two_sum.py
+++ leetcode/two_sum.py
@@ -15,3 +15,19 @@ class Solution:
                     break
                 elif nums[i] + nums[j] == target:
                     return sorted([i, j])
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+        
+        for i in range(len(nums)):
+            need = target - nums[i]
+            if need in seen:
+                return [seen[need], i]
+            seen[nums[i]] = i
