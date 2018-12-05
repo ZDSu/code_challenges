339152596642bdac5bcf7622c4b6cd381fc0bf7c
Beverly Pham <beverly.pham@gmail.com>
2018-12-04 17:10:00 -0800

    add a faster solution

--- leetcode/plusone.py
+++ leetcode/plusone.py
@@ -32,3 +32,24 @@ class Solution:
         else:
             digits[-1] += 1
         return digits
+
+
+# (36 ms)
+class Solution:
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] != 9:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+                if i == 0:
+                    digits.insert(0, 1)
+        return digits
+
+
+# test case: [9] returns [1,0]
