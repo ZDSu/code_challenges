13571d1d148b0b8a12144317b82cccdfbd13b113
Beverly Pham <beverly.pham@gmail.com>
2018-12-05 12:41:09 -0800

    add another solution

--- leetcode/reverse_string.py
+++ leetcode/reverse_string.py
@@ -22,3 +22,20 @@ class Solution:
         :rtype: str
         """
         return s[::-1]
+
+
+# solution 1 = 68 ms; solution 2 = 48 ms; solution 3 = 204 ms
+# not very performant solution
+class Solution:
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s:
+            return s
+        
+        rev = ''
+        for char in s:
+            rev = char + rev
+        return rev
