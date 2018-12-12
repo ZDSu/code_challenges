4c461a95918d1c37ee4290ce855ec6e83c66682e
Beverly Pham <beverly.pham@gmail.com>
2018-12-12 11:10:33 -0800

    add more solutions

--- leetcode/reverse_int.py
+++ leetcode/reverse_int.py
@@ -18,3 +18,42 @@ class Solution:
         if result > 2**31 - 1 or result < -2**31 + 1:
             result = 0
         return result
+
+
+# test case: 1534236469   returns 0
+
+
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        digit = list(str(x))
+        digit.reverse()
+        rev = ''
+        for char in digit:
+            if char == '-':
+                rev = char + rev
+            else:
+                rev += char
+        rev = int(rev)
+        if rev > 2**31 - 1 or rev < -2**31:
+            return 0
+        return rev
+
+
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        x = str(x)
+        if x[0] == '-':
+            x = int(x[0] + x[-1:0:-1])
+        else:
+            x = int(x[::-1])
+        if x > 2**31 - 1 or x < -2**31:
+            return 0
+        return x
