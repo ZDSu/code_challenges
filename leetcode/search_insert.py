cc2f37e6621d79466adce4996b4ffd35b31b7f22
Beverly Pham <beverly.pham@gmail.com>
2019-01-14 22:37:00 -0800

    add another solution

--- leetcode/search_insert.py
+++ leetcode/search_insert.py
@@ -11,4 +11,22 @@ class Solution:
             nums.append(target)
             nums.sort()
         return nums.index(target)
-        
+
+
+class Solution:
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        for i in range(len(nums)):
+            if nums[i] >= target:
+                return i
+        return len(nums)
+
+# test cases:
+# [1], 1  returns 0
+# [1,3], 1  returns 0
