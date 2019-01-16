f6a8e50b1d3d44493a81f42d8436e309f2a10b11
Beverly Pham <beverly.pham@gmail.com>
2019-01-16 12:58:51 -0800

    add more solutions

--- leetcode/length_last_word.py
+++ leetcode/length_last_word.py
@@ -17,3 +17,33 @@ class Solution:
 # ''  returns 0
 # 'a '  returns 1
 # 'b   a    '  returns 1
+
+
+class Solution:
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        words = s.strip().split()
+        if not words:
+            return 0
+        return len(words[-1])
+
+
+class Solution:
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        word = ''
+        if not s:
+            return 0
+
+        s = s.rstrip()
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == ' ':
+                break
+            word += s[i]
+        return len(word)
