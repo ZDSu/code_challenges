a5ee388566d55d62d199097b264d47890c67cf8d
Beverly Pham <beverly.pham@gmail.com>
2019-01-17 19:03:12 -0800

    another solution for plus one

--- leetcode/plusone.py
+++ leetcode/plusone.py
@@ -52,4 +52,27 @@ class Solution:
         return digits
 
 
+class Solution:
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        if not digits:
+            return [1]
+
+        if digits[-1] != 9:
+            digits[-1] += 1
+        else:
+            for i in range(len(digits)-1, -1, -1):
+                if digits[i] == 9:
+                    digits[i] = 0
+                    if i == 0:
+                        digits.insert(0, 1)
+                else:
+                    digits[i] += 1
+                    break
+        return digits
+
+
 # test case: [9] returns [1,0]
