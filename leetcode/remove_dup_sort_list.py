6ef5c280e71e293cf3ce7b836c46b4f8839882c8
Beverly Pham <beverly.pham@gmail.com>
2019-01-18 19:11:21 -0800

    add another solution and some test cases

--- leetcode/remove_dup_sort_list.py
+++ leetcode/remove_dup_sort_list.py
@@ -24,3 +24,36 @@ class Solution:
             else:
                 curr = curr.next
         return head
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return
+
+        curr = head
+        runner = head.next
+
+        while curr.next and runner:
+            if runner.val != curr.val:
+                curr.next = runner
+                curr = curr.next
+            runner = runner.next
+        curr.next = None
+        return head
+
+
+# test cases:
+# [1,1,1]   returns [1]
+# [1,1,2,3,3]
+# []  returns None
