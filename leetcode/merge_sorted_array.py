8d887e3fbdd5e644654c122ff17756a82138b3f8
Beverly Pham <beverly.pham@gmail.com>
2019-01-21 15:48:28 -0800

    add more solutions, best solution

--- leetcode/merge_sorted_array.py
+++ leetcode/merge_sorted_array.py
@@ -41,4 +41,60 @@ class Solution:
 
 # test cases:
 # [0], 0, [1], 1  returns [1]
+# [1], 1, [], 0  returns [1]
 # [2,0], 1, [1], 1 returns [1, 2]
+# [4,5,6,0,0,0], 3, [1,2,3], 3  returns [1,2,3,4,5,6]
+
+
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        if not nums2:
+            return
+
+        m -= 1
+        n -= 1
+        i = len(nums1) - 1
+
+        while n >= 0:
+            if nums1[m] > nums2[n] and m >= 0:
+                nums1[i] = nums1[m]
+                m -= 1
+            else:
+                nums1[i] = nums2[n]
+                n -= 1
+            i -= 1
+
+
+# best solution
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        if not nums2:
+            return
+
+        m -= 1
+        n -= 1
+        i = len(nums1) - 1
+
+        while m >= 0 and n >= 0:
+            if nums1[m] > nums2[n]:
+                nums1[i] = nums1[m]
+                m -= 1
+            else:
+                nums1[i] = nums2[n]
+                n -= 1
+            i -= 1
+        nums1[:n+1] = nums2[:n+1]
