431d7d0d26ecac794d27b0e57383fbffb710476d
Beverly Pham <beverly.pham@gmail.com>
2019-01-22 00:59:23 -0800

    another solution (not really)

--- leetcode/reverse_ll.py
+++ leetcode/reverse_ll.py
@@ -23,3 +23,27 @@ class Solution(object):
             head = temp
         head = previous
         return head
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev = None
+        curr = head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        return prev
