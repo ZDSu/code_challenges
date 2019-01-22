6c2ed3b8fda826bbc7e7e3281138f0d9c6b9370d
Beverly Pham <beverly.pham@gmail.com>
2019-01-21 17:47:54 -0800

    add best solution

--- leetcode/long_common_prefix.py
+++ leetcode/long_common_prefix.py
@@ -45,3 +45,39 @@ class Solution:
 # test cases: 
 # ["aa","a"]   returns "a"
 # [""]   returns ""
+
+
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ''
+
+        prefix = ''
+        for i in range(len(strs[0])):
+            prefix += strs[0][i]
+            for word in strs:
+                if (i > len(word) - 1) or (word[i] != prefix[i]):
+                    return prefix[:-1]
+        return prefix
+
+
+# best solution
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ''
+
+        shortest = min(strs)
+        for i in range(len(shortest)):
+            for word in strs:
+                if word[i] != shortest[i]:
+                    return shortest[:i]
+        return shortest
