5e99f31a2a602a897581227378ec16d943b5312e
Beverly Pham <beverly.pham@gmail.com>
2019-01-21 16:10:22 -0800

    add better solution

--- leetcode/palindrome_number.py
+++ leetcode/palindrome_number.py
@@ -14,3 +14,24 @@ class Solution:
             return True
         else:
             return False
+
+
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        
+        x = str(x)
+        j = len(x) - 1
+        for i in range(len(x) // 2):
+            if x[i] != x[j]:
+                return False
+            j -= 1
+        return True
+
+
+# test case: 11   returns True
