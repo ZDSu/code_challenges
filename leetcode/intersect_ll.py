547f6f2c85040775b3a5beb4678812b5fbba0805
Beverly Pham <beverly.pham@gmail.com>
2019-01-23 19:39:37 -0800

    add another test case and solution

--- leetcode/intersect_ll.py
+++ leetcode/intersect_ll.py
@@ -57,3 +57,68 @@ class Solution(object):
 # test cases:
 # [] []
 # [1] [1]
+
+# test case:  no intersection
+# 0
+# [1,3]
+# [2,4]
+# 2
+# 2
+
+
+# (solution 3)
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if not headA or not headB:
+            return
+
+        currA = headA
+        currB = headB
+
+        while currA != currB:
+            if not currA:
+                currA = headB
+            else:
+                currA = currA.next
+            if not currB:
+                currB = headA
+            else:
+                currB = currB.next
+        return currA
+
+
+# (solution 2)
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if not headA or not headB:
+            return
+        
+        listA = set()
+        while headA:
+            listA.add(headA)
+            headA = headA.next
+        
+        while headB:
+            if headB in listA:
+                return headB
+            headB = headB.next
