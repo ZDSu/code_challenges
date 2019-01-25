9b704a29e861f49219bd79981567e5bb5c04f6dc
Beverly Pham <beverly.pham@gmail.com>
2019-01-24 19:49:18 -0800

    add another solution

--- leetcode/palindrome_linked_list.py
+++ leetcode/palindrome_linked_list.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/palindrome-linked-list/description/
 
 
+# 76 ms, 96%
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -51,4 +52,31 @@ class Solution:
             current = current.next
 
         return list2 == list2[::-1]
-        
+
+
+# 72 ms, 100% (but not O(N) time or O(1) space)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head:
+            return True
+
+        values = []
+        while head:
+            values.append(head.val)
+            head = head.next
+        j = len(values) - 1
+        for i in range(len(values) // 2):
+            if values[i] != values[j]:
+                return False
+            j -= 1
+        return True
