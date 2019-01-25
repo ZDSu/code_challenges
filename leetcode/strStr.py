ec0f74cffd0f536e86bb39fc60d54ad08ac34b48
Beverly Pham <beverly.pham@gmail.com>
2019-01-24 20:28:48 -0800

    add 2 more solutions for strStr

--- leetcode/strStr.py
+++ leetcode/strStr.py
@@ -17,3 +17,41 @@ class Solution:
 
 # or in one line:
 return haystack.find(needle)
+
+
+# 32 ms, 100%
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if not needle:
+            return 0
+
+        length = len(needle)
+        i = 0
+
+        while i + length <= len(haystack):
+            if haystack[i:i + length] == needle:
+                return i
+            i += 1
+        return -1
+
+
+# 52 ms, 45%
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if not needle:
+            return 0
+
+        for i in range(len(haystack)):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
