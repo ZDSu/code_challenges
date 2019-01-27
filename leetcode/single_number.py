63872616a0b0c5e43d2b53047aa78e77ad5e8b8f
Beverly Pham <beverly.pham@gmail.com>
2019-01-26 17:42:50 -0800

    another solution for single number

--- leetcode/single_number.py
+++ leetcode/single_number.py
@@ -17,3 +17,21 @@ class Solution:
         return list(seen.keys())[0]
         # return seen.popitem()[0]   # from solution, but runtime is a lot worse for some weird reason
         # did again and now popitem() is the faster solution
+
+
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return
+
+        seen = set()
+        for num in nums:
+            if num in seen:
+                seen.remove(num)
+            else:
+                seen.add(num)
+        return seen.pop()
