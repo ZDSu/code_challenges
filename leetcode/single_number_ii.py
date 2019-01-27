4c0aa68828450769bb45e2cdbcfd046520c81832
Beverly Pham <beverly.pham@gmail.com>
2019-01-26 18:03:26 -0800

    single number ii done again

--- leetcode/single_number_ii.py
+++ leetcode/single_number_ii.py
@@ -17,3 +17,22 @@ class Solution:
             else:
                 seen[num] = 1
         return seen.popitem()[0]
+
+
+# same time as above: 36 ms, 100%
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        seen ={}
+        for num in nums:
+            if num in seen:
+                if seen[num] == 2:
+                    del seen[num]
+                else:
+                    seen[num] += 1
+            else:
+                seen[num] = 1
+        return list(seen.keys())[0]
