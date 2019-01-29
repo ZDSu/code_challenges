6cdd4f2eb194212c953aa2b98cb1be8c48b69dc0
Beverly Pham <beverly.pham@gmail.com>
2019-01-28 22:49:09 -0800

    another solution for merge sorted linked list

--- leetcode/merge_sorted_ll.py
+++ leetcode/merge_sorted_ll.py
@@ -37,5 +37,29 @@ class Solution:
         elif l2:
             merged.next = l2
         return head.next
-            
-            
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        head = merged = ListNode(None)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                merged.next = l1
+                l1 = l1.next
+            else:
+                merged.next = l2
+                l2 = l2.next
+            merged = merged.next
+        merged.next = l1 or l2
+        return head.next
