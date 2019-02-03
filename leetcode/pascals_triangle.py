ff65b32adf446f3bdfd73a705bdb7fc37b7fc87d
Beverly Pham <beverly.pham@gmail.com>
2019-02-03 14:09:23 -0800

    pascals triangle solved better, 100%

--- leetcode/pascals_triangle.py
+++ leetcode/pascals_triangle.py
@@ -19,3 +19,24 @@ class Solution:
             res.append(row)
 
         return res
+
+
+class Solution:
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        triangle = []
+        if numRows > 0:
+            triangle.append([1])
+
+        for i in range(1, numRows):
+            row = [None] * (i + 1)
+            row[0] = 1
+            row[i] = 1
+            if i > 1:
+                for j in range(1, i):
+                    row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+            triangle.append(row)
+        return triangle
