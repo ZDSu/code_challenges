cd391b179d9ecc00633080c378ca49e9e656e69e
Beverly Pham <beverly.pham@gmail.com>
2019-02-05 14:26:46 -0800

    add another solution for lemonade change and someone else's faster solution

--- leetcode/lemonade_change.py
+++ leetcode/lemonade_change.py
@@ -31,6 +31,7 @@ class Solution:
 
 
 # from solution, takes half the runtime
+# 44 ms, 6.8 MB, 100%
 class Solution:
     def lemonadeChange(self, bills):
         """
@@ -54,4 +55,58 @@ class Solution:
                     five -= 3
                 else:
                     return False
+        return True
+
+
+# 44 ms, 6.8 MB, 100% (someone else's solution)
+class Solution:
+    def lemonadeChange(self, bills):
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
+        five = ten = 0
+        for num in bills:
+            if num == 5:
+                five += 1
+            elif num == 10 and five:
+                ten += 1
+                five -= 1
+            elif num == 20 and five and ten:
+                five -= 1
+                ten -= 1
+            elif num == 20 and five >= 3:
+                five -= 3
+            else:
+                return False
+        return True
+
+
+# 48 ms, 6.9 MB, 75%
+class Solution:
+    def lemonadeChange(self, bills):
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
+        money = {5: 0, 10: 0, 20: 0}
+
+        for bill in bills:
+            if bill == 5:
+                money[5] += 1
+            elif bill == 10:
+                if money[5] > 0:
+                    money[5] -= 1
+                    money[10] += 1
+                else:
+                    return False
+            else:  # bill == 20:
+                if money[5] > 0 and money[10] > 0:
+                    money[10] -= 1
+                    money[5] -= 1
+                elif money[5] > 2:
+                    money[5] -= 3
+                else:
+                    return False
+                money[20] += 1
         return True
