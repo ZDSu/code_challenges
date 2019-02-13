f1d0ea2bf2868879d7d54bfd9b41afd58516bdbd
Beverly Pham <beverly.pham@gmail.com>
2019-02-12 18:16:43 -0800

    add a divide and conquer solution for reverse string

--- leetcode/reverse_string.py
+++ leetcode/reverse_string.py
@@ -39,3 +39,18 @@ class Solution:
         for char in s:
             rev = char + rev
         return rev
+
+
+# runtime: 200 ms, 29%; memory: 18.5 MB, 1%
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: void Do not return anything, modify s in-place instead.
+        """
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
