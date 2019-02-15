cc18a548c98312e3bb42e3856b7f83f0867bca12
Beverly Pham <beverly.pham@gmail.com>
2019-02-15 13:03:32 -0800

    add functions to insert left and right children

--- dsa/trees/tree_class.py
+++ dsa/trees/tree_class.py
@@ -10,4 +10,20 @@ class BinaryTree:
         self.leftChild = None
         self.rightChild = None
 
+    def insertLeft(self, node):
+        """Insert new node as left child."""
+        if not self.leftChild:
+            self.leftChild = BinaryTree(node)
+        else:
+            root = BinaryTree(node)
+            root.leftChild = self.leftChild
+            self.leftChild = root
 
+    def insertRight(self, node):
+        """Insert new node as right child."""
+        if not self.rightChild:
+            self.rightChild = BinaryTree(node)
+        else:
+            root = BinaryTree(node)
+            root.rightChild = self.rightChild
+            self.rightChild = root
