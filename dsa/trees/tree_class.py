88e9652568580b2431f13d2504fddd6fc8286dac
Beverly Pham <beverly.pham@gmail.com>
2019-02-15 12:58:42 -0800

    start tree class

---
+++ dsa/trees/tree_class.py
@@ -0,0 +1,13 @@
+"""Class definition of a tree."""
+
+
+class BinaryTree:
+    """Class definition of a binary tree."""
+
+    def __init__(self, rootObj):
+        """Initialization."""
+        self.key = rootObj
+        self.leftChild = None
+        self.rightChild = None
+
+
