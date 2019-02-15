1d3e9420ee736792b8a881fdaa5370c52a474e1d
Beverly Pham <beverly.pham@gmail.com>
2019-02-15 12:45:40 -0800

    start tree section, define tree using lists

---
+++ dsa/trees/tree_list_representation.py
@@ -0,0 +1,42 @@
+"""Representation of a tree using a list of lists."""
+
+
+def BinaryTree(r):
+    """
+    Define root of binary tree.
+
+    r is the root at index 0 followed by its left and right children at indices 1 and 2, respectively.
+    """
+    return [r, [], []]
+
+
+def insertLeft(root, newBranch):
+    """
+    Add a left child to node.
+
+    If left child exists, add new left child by making the existing left child the left child in the newly inserted list.
+    """
+    t = root.pop(1)  # obtain the left child list, which is at index 1
+
+    if len(t) > 1:  # if there is an existing left child
+        root.insert(1, [newBranch, t, []])
+    else:
+        root.insert(1, [newBranch, [], []])
+
+    return root
+
+
+def insertRight(root, newBranch):
+    """
+    Add a right child to node.
+
+    If right child exists, add new right child by making the existing right child the right child in the newly inserted list.
+    """
+    t = root.pop(2)  # obtain the right child list, which is at index 2
+
+    if len(t) > 1:  # if there is an existing right child
+        root.insert(2, [newBranch, [], t])
+    else:
+        root.insert(2, [newBranch, [], []])
+
+    return root
