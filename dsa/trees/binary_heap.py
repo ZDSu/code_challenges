ee2cb11c191d17181ba5fe8b78a342a283d86d10
Beverly Pham <beverly.pham@gmail.com>
2019-02-15 18:22:08 -0800

    binary heap list representation constructor

---
+++ dsa/trees/binary_heap.py
@@ -0,0 +1,10 @@
+"""Binary Heap Implementation."""
+
+
+class BinaryHeap:
+    """Binary Heap class."""
+
+    def __init__(self):
+        """Constructor."""
+        self.heapList = [0]
+        self.size = 0
