26577a00ad1378427da93b31dd5d6d6e9b6e8608
Beverly Pham <beverly.pham@gmail.com>
2019-02-15 18:43:03 -0800

    insert method using percup method written

--- dsa/trees/binary_heap.py
+++ dsa/trees/binary_heap.py
@@ -8,3 +8,20 @@ class BinaryHeap:
         """Constructor."""
         self.heapList = [0]
         self.size = 0
+
+    def percUp(self, index):
+        """Percolate up."""
+        while index // 2 > 0:
+            if self.heapList[index] < self.heapList[index // 2]:
+                # temp = self.heapList[index // 2]
+                # self.heapList[i // 2] = self.heapList[index]
+                # self.heapList[i] = temp
+                self.heapList[index // 2], self.heapList[index] = self.heapList[index], self.heapList[index // 2]
+            index = index // 2
+
+    def insert(self, key):
+        """Insert new node in heap."""
+        self.heapList.append(key)
+        self.size += 1
+        self.percUp(self.size)
+
