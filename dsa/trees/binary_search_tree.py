08432e5f8e113107ae2cd9e11555bb535f59cce6
Beverly Pham <beverly.pham@gmail.com>
2019-02-16 15:39:31 -0800

    write insert method

--- dsa/trees/binary_search_tree.py
+++ dsa/trees/binary_search_tree.py
@@ -1,6 +1,9 @@
 """Binary search tree class."""
 
 
+from tree_node import Node
+
+
 class BinarySearchTree:
     """Binary search tree class."""
 
@@ -20,3 +23,24 @@ class BinarySearchTree:
     def __iter__(self):
         """Iterate through the binary search tree, called on root."""
         return self.root.__iter__()
+
+    def insert(self, key, value):
+        """Insert a new node into tree."""
+        if self.root:
+            self._insert(key, value, self.root)
+        else:
+            self.root = Node(key, value)
+        self.size += 1
+
+    def _insert(self, key, value, currentNode):
+        """Insert helper method."""
+        if key < currentNode.key:
+            if currentNode.hasLeftChild():
+                self._insert(key, value, currentNode.leftChild)
+            else:
+                currentNode.leftChild = Node(key, value, parent=currentNode)
+        else:
+            if currentNode.hasRightChild():
+                self._insert(key, value, currentNode.rightChild)
+            else:
+                currentNode.rightChild = Node(key, value, parent=currentNode)
