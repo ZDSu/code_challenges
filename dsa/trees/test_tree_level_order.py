f9d0cd3bf38622678dc5767a509634df600e1af1
Beverly Pham <beverly.pham@gmail.com>
2019-02-16 18:28:12 -0800

    print tree level order exercise and test files made

---
+++ dsa/trees/test_tree_level_order.py
@@ -0,0 +1,8 @@
+"""Test cases for print tree level order."""
+
+from tree_level_order import *
+
+
+def test_tree_level():
+    """Test."""
+    pass
---
+++ dsa/trees/tree_level_order.py
@@ -0,0 +1,15 @@
+"""Given a binary tree of integers, print it in level order. The output will contain space between the numbers in the same level, and new line between different levels."""
+
+
+class Node:
+    """Node class for binary tree."""
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val 
+
+
+def levelOrderPrint(tree):
+    """Print nodes in level order. Print each level on one line."""
+    # Code here
+    pass
