ebd748939d69205e00ca39688511391747c263bd
Beverly Pham <beverly.pham@gmail.com>
2019-02-16 18:30:39 -0800

    trim bst exercise and test files made

---
+++ dsa/trees/test_trim_bst.py
@@ -0,0 +1,8 @@
+"""Test cases for trim BST."""
+
+from trim_bst import *
+
+
+def test_trim():
+    """Test trim BST."""
+    pass
---
+++ dsa/trees/trim_bst.py
@@ -0,0 +1,12 @@
+"""Given the root of a binary search tree and 2 numbers min and max, trim the tree such that all the numbers in the new tree are between min and max (inclusive). The resulting tree should still be a valid binary search tree."""
+
+
+def trimBST(tree, minVal, maxVal):
+
+    print tree.left  # LeftChild
+    print tree.right  # Right Child
+    print tree.val  # Node's value
+
+    pass
+
+# Use tree.left, tree.right, and tree.val as your methods to call
