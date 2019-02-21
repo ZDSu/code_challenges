f3708ff5a709d980b07a207701d33346cf92db2d
Beverly Pham <beverly.pham@gmail.com>
2019-02-20 20:13:21 -0800

    inorder traversal solved iteratively

--- leetcode/bt_inorder.py
+++ leetcode/bt_inorder.py
@@ -30,3 +30,35 @@ class Solution(object):
 
         travel(root)
         return res
+
+
+# (iterative) runtime 20 ms, 100%; memory 10.9 MB, 6%
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        stack = []
+
+        if not root:
+            return res
+
+        while root or stack:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                root = stack.pop()
+                res.append(root.val)
+                root = root.right
+
+        return res
