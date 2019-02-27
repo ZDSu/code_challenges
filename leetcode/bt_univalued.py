2aa412f584c0a143b86766173e7783a82ff9e821
Beverly Pham <beverly.pham@gmail.com>
2019-02-26 17:26:49 -0800

    univalued binary tree solved recursively

--- leetcode/bt_univalued.py
+++ leetcode/bt_univalued.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/univalued-binary-tree/
+# https://leetcode.com/articles/univalued-binary-tree/
 
 
 # runtime: 16 ms, 100%; memory 10.8 MB, 75%
@@ -27,3 +28,27 @@ class Solution(object):
             if curr.right:
                 queue.append(curr.right)
         return True
+
+
+# (recursive) runtime: 16 ms, 100%; memory 10.9 MB, 16%
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isUnivalTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def univalued(root, last_val):
+            if not root:
+                return True
+            if root.val != last_val:
+                return False
+            return univalued(root.left, root.val) and univalued(root.right, root.val)
+
+        return univalued(root, root.val)
