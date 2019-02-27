2a54194aca5d79c23fe7b01b6774ef3efe10d4b9
Beverly Pham <beverly.pham@gmail.com>
2019-02-26 21:47:51 -0800

    add better anagram solution for interviewing

--- fcc_pyalgo/anagrams.py
+++ fcc_pyalgo/anagrams.py
@@ -11,6 +11,38 @@ def anagram(s1, s2):
     # Return boolean for sorted match.
     return sorted(s1) == sorted(s2)
 
+
+def anagram(s1, s2):
+    s1 = s1.replace(' ', '').lower()
+    s2 = s2.replace(' ', '').lower()
+
+    # check if same number of letters
+    if len(s1) != len(s2):
+        return False
+
+    # count frequency of each letter
+    count = {}
+
+    # count the letters in s1 using a dictionary
+    for letter in s1: 
+        if letter in count:
+            count[letter] += 1
+        else:
+            count[letter] = 1
+
+    # do reverse for second string
+    for letter in s1:
+        if letter in count:
+            count[letter] -= 1
+        else:
+            count[letter] = 1
+
+    for k in count:
+        if count[k] != 0:
+            return False
+
+    return True
+
 anagram('dog', 'god')  # True
 anagram('clint eastwood', 'old west action')  # True
 anagram('public relations', 'crap built on lies')  # True
