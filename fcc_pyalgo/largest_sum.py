1221f316d44b15afb5222972b56cd254e9e451a8
Beverly Pham <beverly.pham@gmail.com>
2019-03-02 13:47:52 -0800

    add fcc solution for largest sum

--- fcc_pyalgo/largest_sum.py
+++ fcc_pyalgo/largest_sum.py
@@ -18,3 +18,19 @@ def largest_sum(nums):
         if temp > largest:
             largest = temp
     return largest
+
+
+# youtube solution--slower but slightly less memory used
+def largest_sum(nums):
+    if not nums:
+        return
+
+    largest = temp = nums[0]
+
+    for num in nums[1:]:
+        temp = max(temp + num, num)
+        largest = max(temp, largest)
+
+    return largest
+
+print(largest_sum([7, 1, 2, -1, 3, 4, 10, -12, 3, 21, -19]))  # returns 38
