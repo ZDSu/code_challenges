da0571c4a35a6327df082e38b51d52c9f2ab0feb
Beverly Pham <beverly.pham@gmail.com>
2019-03-06 18:45:57 -0800

    k-ary tree level order traversal solved, try to make more performant

--- leetcode/k_ary_tree_level_order.py
+++ leetcode/k_ary_tree_level_order.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/n-ary-tree-level-order-traversal/
 
 
+# runtime 124 ms, 14%; memory 17.6 MB, 5.5%
 """
 # Definition for a Node.
 class Node:
@@ -10,13 +11,19 @@ class Node:
 """
 class Solution:
     def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return []
+
         traverse = []
         queue = [root]
 
         while queue:
-            curr = queue.pop(0)
-            traverse.append(curr.val)
-            if curr.children:
-                for child in curr.children:
-                    queue.append(child)
+            level = []
+            for _ in range(len(queue)):
+                curr = queue.pop(0)
+                level.append(curr.val)
+                if curr.children:
+                    for child in curr.children:
+                        queue.append(child)
+            traverse.append(level)
         return traverse
