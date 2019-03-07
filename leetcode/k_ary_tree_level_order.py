cbfa6ed4e905d33b0bdcd6a8693b0e0f869d66cc
Beverly Pham <beverly.pham@gmail.com>
2019-03-06 19:29:47 -0800

    add sample solutions that were not better

--- leetcode/k_ary_tree_level_order.py
+++ leetcode/k_ary_tree_level_order.py
@@ -30,6 +30,9 @@ class Solution:
 
 
 # runtime 108 ms, 28%; memory 17.7 MB, 5.5%
+# ran again after solutions below and got:
+# runtime 100 ms, 50%; memory 17.6 MB, 5.5%
+
 class Solution:
     def levelOrder(self, root: 'Node') -> List[List[int]]:
         if not root:
@@ -53,3 +56,42 @@ class Solution:
 # test cases:
 # null
 # {"$id":"1","children":[],"val":44}
+
+
+# other solutions that are supposedly faster but are not:
+# sample 88 ms submission; when I ran this code, I got:
+# runtime 120 ms, 18.5%; memory 17.9 MB, 5.5%
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        ans = []
+        base = [root]
+        while base:
+            nb = []
+            cur = []
+            for i in base:
+                if i:
+                    cur.append(i.val)
+                    nb += i.children
+            if cur:
+                ans.append(cur)
+            base = nb
+        return ans
+
+# sample 96 ms submission; when I ran this code, I got:
+# runtime 100 ms, 50%; memory 17.4 MB, 5.5%
+# then I got: runtime 100 ms, 50%; memory 17.7 MB, 5.5%
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        cur = [root]
+
+        while cur:
+            res.append([node.val for node in cur])
+            tmp = []
+            for node in cur:
+                tmp += node.children
+            cur = tmp
+
+        return res
