6cd4ce435937b843dbeb92181f243a93b45284fc
Beverly Pham <beverly.pham@gmail.com>
2019-03-06 19:29:18 -0800

    better solution for k-ary tree level order traversal

--- leetcode/k_ary_tree_level_order.py
+++ leetcode/k_ary_tree_level_order.py
@@ -18,12 +18,38 @@ class Solution:
         queue = [root]
 
         while queue:
-            level = []
+            level = kids = []
             for _ in range(len(queue)):
                 curr = queue.pop(0)
                 level.append(curr.val)
                 if curr.children:
-                    for child in curr.children:
-                        queue.append(child)
+                    kids.append(children)
             traverse.append(level)
+            queue = kids
         return traverse
+
+
+# runtime 108 ms, 28%; memory 17.7 MB, 5.5%
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return []
+
+        traverse = []
+        queue = [root]
+
+        while queue:
+            level = []
+            kids = []
+            for node in queue:
+                level.append(node.val)
+                if node.children:
+                    kids += node.children
+            traverse.append(level)
+            queue = kids
+        return traverse
+
+
+# test cases:
+# null
+# {"$id":"1","children":[],"val":44}
