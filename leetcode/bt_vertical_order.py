e20f60a8ee8f8c42d3fa2b17b23a3b0eb917da49
Beverly Pham <beverly.pham@gmail.com>
2019-03-06 22:28:50 -0800

    still working on vertical order bt traversal

--- leetcode/bt_vertical_order.py
+++ leetcode/bt_vertical_order.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
-
+# this challenge is #987. #314 is similar. Difference is:
+#  314: If two nodes are in the same row and column, the order should be from left to right. (also only for premium users)
+# 987: If two nodes have the same position, then the value of the node that is reported first is the value that is smaller.
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -50,6 +52,7 @@ class Solution:
 # test cases:
 # [0,2,1,3,null,null,null,4,5,null,7,6,null,10,8,11,9]  returns [[4,10,11],[3,6,7],[2,5,8,9],[0],[1]]
 # getting [[4,10,11],[3,7,6],[2,5,8,9],[0],[1]]
+# [0,5,1,9,null,2,null,null,null,null,3,4,8,6,null,null,null,7] returns [[9,7],[5,6],[0,2,4],[1,3],[8]]
 
 print(ht[key])
 [(-3, -3, 4), (-3, -5, 10), (-3, -5, 11)]
@@ -64,3 +67,40 @@ print(temp)
 [2, 5, 8, 9]
 [0]
 [1]
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def verticalTraversal(self, root: TreeNode) -> List[List[int]]:
+        res = []
+        ht = {}
+        root.val = (0, 0, root.val)
+        queue = [root]
+
+        while queue:
+            curr = queue.pop(0)
+            x = curr.val[0]
+            y = curr.val[1]
+            if x in ht:
+                ht[x].append(curr.val)
+            else:
+                ht[x] = [curr.val]
+            if curr.left:
+                curr.left.val = (x-1, y-1, curr.left.val)
+                queue.append(curr.left)
+            if curr.right:
+                curr.right.val = (x+1, y-1, curr.right.val)
+                queue.append(curr.right)
+        for key in sorted(ht.keys()):
+            temp = []
+            for node in ht[key]:
+                temp.append(node[2])
+            res.append(sorted(temp))
+        return res
+
+# doesn't pass : [0,5,1,9,null,2,null,null,null,null,3,4,8,6,null,null,null,7]
