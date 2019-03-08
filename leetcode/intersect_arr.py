fd0214dcaee95185f2d527fc3fa1754abd75c83b
Beverly Pham <beverly.pham@gmail.com>
2019-03-07 17:50:29 -0800

    add another solution for intersection of 2 arrays

--- leetcode/intersect_arr.py
+++ leetcode/intersect_arr.py
@@ -11,3 +11,22 @@ class Solution:
         nums1 = set(nums1)
         nums2 = set(nums2)
         return list(nums1.intersection(nums2))
+
+
+# runtime 40 ms, 74%; memory 13.2 MB, 5%
+class Solution:
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        if not nums1 or not nums2:
+            return []
+
+        ans = set()
+        nums1 = set(nums1)
+        for num in nums2:
+            if num in nums1:
+                ans.add(num)
+        return list(ans)
