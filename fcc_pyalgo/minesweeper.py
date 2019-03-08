18bdc4e3af43058d0d0f60acf6dfabfc5fe12a88
Beverly Pham <beverly.pham@gmail.com>
2019-03-07 20:09:42 -0800

    start fcc minesweeper

---
+++ fcc_pyalgo/minesweeper.py
@@ -0,0 +1,18 @@
+"""
+Write a function that wil take 3 arguments:
+bombs = list of bomb locations
+rows, columns
+mine_sweeper([[0,0], [1,2], 3, 4])
+
+bomb at row index 0 column index 0
+bomb at row index 0 column index 1
+3 rows
+3 columns
+
+we should return a 3 x 4 array (-1) = bomb
+(ends up deleting this) [[-1, -1, 1, 0], [2, 2, 1, 0], the 2 bombs means 2 bombs in surrounding cells [1,0] knows [0,0] and [0,1] have    0,0,0,0]]
+similar: https://leetcode.com/problems/minesweeper/
+"""
+
+def minesweeper(bombs, rows, columns):
+    pass
