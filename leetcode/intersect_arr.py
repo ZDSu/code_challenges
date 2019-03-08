39a1c3cc228aa5973925f46b38eff80584ddec87
Beverly Pham <beverly.pham@gmail.com>
2019-03-07 17:51:03 -0800

    intersectino of 2 arrays with a list, for comparison purposes

--- leetcode/intersect_arr.py
+++ leetcode/intersect_arr.py
@@ -30,3 +30,22 @@ class Solution:
             if num in nums1:
                 ans.add(num)
         return list(ans)
+
+
+# runtime 60 ms, 30%; memory 13.1 MB, 5%
+class Solution:
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        if not nums1 or not nums2:
+            return []
+
+        res = []
+        nums1 = set(nums1)
+        for num in nums2:
+            if num in nums1:
+                res.append(num)
+        return list(set(res))
