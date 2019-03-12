9db613084bb556012cddff4b28e523d9391f55f7
Beverly Pham <beverly.pham@gmail.com>
2019-03-12 15:24:05 -0700

    fcc unique char in string, add fcc solution using built-in methods

--- fcc_pyalgo/09_unique_char_in_strings.py
+++ fcc_pyalgo/09_unique_char_in_strings.py
@@ -7,6 +7,7 @@ Remember, arrays are mutable but strings are immutable
 
 Similar: https://leetcode.com/problems/first-unique-character-in-a-string/
 """
+# my solution, without clarifications
 def unique(s):
     chars = set()
 
@@ -17,4 +18,15 @@ def unique(s):
 
     return True
 
-print(unique('abc'))
+
+# using python built-in structures
+def unique(s):
+    # clean up input string, take out spaces, make everything lowercase, take out punctuation -- depending on the problem domain and input possibilities
+    s = s.replace(' ', '')
+
+    # use a set (an unordered collection of unique elements)
+    return len(set(s)) == len(s)
+
+
+print(unique('a b cdef'))  # True
+print(unique('a b cdeaf'))  # False
