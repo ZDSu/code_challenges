fbcef7de9e4b5bf017b38dd18a6e5d580b3afb45
Beverly Pham <beverly.pham@gmail.com>
2019-03-12 15:53:12 -0700

    fcc non repeat element solved for first unique char

---
+++ fcc_pyalgo/10_non_repeat_element.py
@@ -0,0 +1,27 @@
+"""
+Non-Repeat Elements in Array
+Take a string and return character that never repeats (Note: the title says array but his directions say string)
+if multiple uniques then return only the FIRST unique
+Then will also solve to return EVERY element that is not repeating
+
+https://leetcode.com/problems/first-unique-character-in-a-string/ (same challenge as for #9)
+"""
+
+
+def nonrepeat(s):
+    s = s.replace(' ', '').lower()
+    chars = {}
+
+    for letter in s:
+        if letter in chars:
+            chars[letter] += 1
+        else:
+            chars[letter] = 1
+
+    for letter in s:
+        if chars[letter] == 1:
+            return c
+
+    return
+
+print(nonrepeat('I Apple Ape Peels'))  # returns i
