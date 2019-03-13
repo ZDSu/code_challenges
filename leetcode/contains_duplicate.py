97016e4e620cccb53cbefa7009e9d96533c45a6f
Beverly Pham <beverly.pham@gmail.com>
2019-03-13 14:24:03 -0700

    add another one-liner for contains duplicate

--- leetcode/contains_duplicate.py
+++ leetcode/contains_duplicate.py
@@ -34,3 +34,13 @@ class Solution:
         :rtype: bool
         """
         return not len(nums) == len(set(nums))
+
+
+# runtime 48 ms, 66%; memory 18.6 MB, 26%
+class Solution:
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(nums) != len(set(nums))
