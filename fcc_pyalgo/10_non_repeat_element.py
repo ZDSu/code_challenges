455404a5669f2e780575dc105413f8b3582a86b9
Beverly Pham <beverly.pham@gmail.com>
2019-03-12 23:43:23 -0700

    fcc non repeat element solved for all non-uniques

--- fcc_pyalgo/10_non_repeat_element.py
+++ fcc_pyalgo/10_non_repeat_element.py
@@ -25,3 +25,28 @@ def first_nonrepeat(s):
     return
 
 print(first_nonrepeat('I Apple Ape Peels'))  # returns i
+
+
+def nonrepeats(s):
+    s = s.replace(' ', '').lower()
+    chars = {}
+    uniques = []
+
+    for letter in s:
+        if letter in chars:
+            chars[letter] += 1
+        else:
+            chars[letter] = 1
+
+    y = sorted(chars.items(), key=lambda x: x[1])
+
+    for item in y:
+        # if item[1] == y[0][1]:
+        if item[1] == 1:
+            uniques.append(item[0])
+
+    return uniques
+
+# Note, his code is wrong (line 44). this will return all elements that occur the same amount of times as the least occuring element. For example, nonrepeats('Is Applie Ape Peels') will return ['i', 's', 'a', 'l']
+
+print(nonrepeats('I Apple Ape Peels'))  # returns [('i', 1), ('s', 1)]
