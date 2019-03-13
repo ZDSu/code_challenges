0c835423fbb48d53876fb9d2778f6baf101e0d3c
Beverly Pham <beverly.pham@gmail.com>
2019-03-13 14:59:59 -0700

    sort characters by frequency solved another way

--- leetcode/sort_char_by_frequency.py
+++ leetcode/sort_char_by_frequency.py
@@ -14,3 +14,21 @@ class Solution:
             res += item[0] * item[1]
 
         return res
+
+
+# runtime 64 ms, 41%; memory 13.6 MB, 17%
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        res = ''
+        count = {}
+
+        for char in s:
+            if char in count:
+                count[char] += 1
+            else:
+                count[char] = 1
+
+        for item in sorted(count.items(), key=lambda x: x[1], reverse=True):
+            res += item[0] * item[1]
+
+        return res
