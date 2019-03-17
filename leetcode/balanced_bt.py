fb7ce8199533e077ecb278ccfd6ea0574d1f1bfd
Beverly Pham <beverly.pham@gmail.com>
2019-03-17 00:43:06 -0700

    working on balanced binary tree

--- leetcode/balanced_bt.py
+++ leetcode/balanced_bt.py
@@ -10,38 +10,41 @@
 
 class Solution:
     def isBalanced(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+
         left = 0
         right = 0
 
         if root.left:
             queue1 = [root.left]
+
+            while queue1:
+                level1 = []
+                curr = queue1.pop(0)
+                if curr.left or curr.right:
+                    left += 1
+                    if curr.left:
+                        level1.append(curr.left)
+                    if curr.right:
+                        level1.append(curr.right)
+                if level1:
+                    queue1 += level1
+
         if root.right:
             queue2 = [root.right]
+            while queue2:
+                level2 = []
 
-        while queue1:
-            level1 = []
-            curr = queue1.pop(0)
-            if curr.left or curr.right:
-                left += 1
-                if curr.left:
-                    level1.append(curr.left)
-                if curr.right:
-                    level1.append(curr.right)
-            if level1:
-                queue1 += level1
-
-        while queue2:
-            level2 = []
-
-            curr = queue2.pop(0)
-            if curr.left or curr.right:
-                right += 1
-                if curr.left:
-                    level2.append(curr.left)
-                if curr.right:
-                    level2.append(curr.right)
-            if level2:
-                queue2 += level2
+                curr = queue2.pop(0)
+                if curr.left or curr.right:
+                    right += 1
+                    if curr.left:
+                        level2.append(curr.left)
+                    if curr.right:
+                        level2.append(curr.right)
+                if level2:
+                    queue2 += level2
 
         return abs(left - right) <= 1
 
@@ -49,3 +52,9 @@ class Solution:
 # [3,9,5,20,null,3,null,15,3,null,4,2,3,7]  says false and output false but should be true
 # [3,9,5,20,null,null,15,3,null,4,2,3,7]  says false, but outputs true and should be true
 # [3,9,5,20,null,null,15,3,null,4,2,3,7]  says false, but outputs true and should be true
+
+
+# test cases:
+# []  returns true
+# [1]  returns true
+# [1,null,2,null,3] returns false
