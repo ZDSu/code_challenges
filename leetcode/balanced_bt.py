e3506aed3865dc7f7f750ec0c86e29e62c9cfc44
Beverly Pham <beverly.pham@gmail.com>
2019-03-17 00:34:15 -0700

    still working on balanced binary tree

--- leetcode/balanced_bt.py
+++ leetcode/balanced_bt.py
@@ -9,25 +9,43 @@
 #         self.right = None
 
 class Solution:
-    def isBalanced(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
+    def isBalanced(self, root: TreeNode) -> bool:
         left = 0
         right = 0
 
-        queue = [root]
+        if root.left:
+            queue1 = [root.left]
+        if root.right:
+            queue2 = [root.right]
 
-        while queue:
-            level = []
-            curr = queue.pop(0)
-            if curr.left:
-                level.append(curr.left)
+        while queue1:
+            level1 = []
+            curr = queue1.pop(0)
+            if curr.left or curr.right:
                 left += 1
-            if curr.right:
-                level.append(curr.right)
+                if curr.left:
+                    level1.append(curr.left)
+                if curr.right:
+                    level1.append(curr.right)
+            if level1:
+                queue1 += level1
+
+        while queue2:
+            level2 = []
+
+            curr = queue2.pop(0)
+            if curr.left or curr.right:
                 right += 1
-            queue.append(level)
+                if curr.left:
+                    level2.append(curr.left)
+                if curr.right:
+                    level2.append(curr.right)
+            if level2:
+                queue2 += level2
 
         return abs(left - right) <= 1
+
+# my test cases (if tree visualizer is true):
+# [3,9,5,20,null,3,null,15,3,null,4,2,3,7]  says false and output false but should be true
+# [3,9,5,20,null,null,15,3,null,4,2,3,7]  says false, but outputs true and should be true
+# [3,9,5,20,null,null,15,3,null,4,2,3,7]  says false, but outputs true and should be true
