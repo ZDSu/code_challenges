a043db6e613c187dccb52c4591012cb94ffc1a47
Beverly Pham <beverly.pham@gmail.com>
2019-03-18 22:28:40 -0700

    maximum depth of n-ary tree solved

---
+++ leetcode/max_depth_nary_tree.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+
+
+# runtime 100 ms, 18%; memory 17.7 MB, 5%
+# then ran again, runtime 88 ms, 65%; memory 17.6 MB, 5%
+# then ran again, runtime 92 ms, 42%; memory 17.7 MB, 5%
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+
+        depth = 0
+        queue = [root]
+
+        while queue:
+            level = []
+            depth += 1
+            for _ in range(len(queue)):
+                curr = queue.pop(0)
+                if curr.children:
+                    level += curr.children
+            if level:
+                queue += level
+
+        return depth
