a0daa67999280cdad1323c4e767acf2c74c13661
Beverly Pham <beverly.pham@gmail.com>
2019-03-23 15:46:39 -0700

    another solution for binary search

--- leetcode/binary_search.py
+++ leetcode/binary_search.py
@@ -20,3 +20,29 @@ class Solution:
             else:
                 low = mid + 1
         return -1
+
+
+# runtime: 48 ms, 91%; memory 14.1 MB, 5%
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return -1
+
+        start = 0
+        end = len(nums) - 1
+
+        while start <= end:
+            mid = (start + end) // 2
+            print(mid)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid - 1
+        return -1
