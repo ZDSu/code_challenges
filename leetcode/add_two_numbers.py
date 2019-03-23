ddee3c70e49f84fb4de47bcf2ee3d758bb7f253e
Beverly Pham <beverly.pham@gmail.com>
2019-03-23 13:19:51 -0700

    another solution for add two numbers

--- leetcode/add_two_numbers.py
+++ leetcode/add_two_numbers.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/add-two-numbers/
 
+# instuctions should clarify that l1 and l2 can have different lengths
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -7,24 +8,50 @@
 #         self.val = x
 #         self.next = None
 
-# passes 729/1563 tests; assumed length of l1 and l2 would be same (did not indicate either way)
+# runtime: 120 ms, 34%; memory 13.4 MB, 5%
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         num1 = num2 = ''
 
-        while l1 and l2:
+        while l1:
             num1 = str(l1.val) + num1
+            l1 = l1.next
+        while l2:
             num2 = str(l2.val) + num2
-            l1, l2 = l1.next, l2.next
+            l2 = l2.next
         total = str(int(num1) + int(num2))
-        
+
         l3 = ListNode(int(total[-1]))
         dummy = ListNode(0)
         dummy.next = l3
         for i in range(len(total)-2, -1, -1):
             l3.next = ListNode(int(total[i]))
             l3 = l3.next
-            
+
+        return dummy.next
+
+
+# runtime: 120 ms, 34%; memory 13.3 MB, 5%
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        num1 = num2 = ''
+
+        while l1 or l2:
+            if l1:
+                num1 = str(l1.val) + num1
+                l1 = l1.next
+            if l2:
+                num2 = str(l2.val) + num2
+                l2 =  l2.next
+        total = str(int(num1) + int(num2))
+
+        l3 = ListNode(int(total[-1]))
+        dummy = ListNode(0)
+        dummy.next = l3
+        for i in range(len(total)-2, -1, -1):
+            l3.next = ListNode(int(total[i]))
+            l3 = l3.next
+
         return dummy.next
 
 # test case: [1,8], [0]   returns [1,8]
