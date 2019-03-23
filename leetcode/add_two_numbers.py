ad4d55eb79203ce5d79696cbfb201414acd2a1b5
Beverly Pham <beverly.pham@gmail.com>
2019-03-23 13:13:07 -0700

    working on add two numbers, instructions unclear

--- leetcode/add_two_numbers.py
+++ leetcode/add_two_numbers.py
@@ -7,9 +7,9 @@
 #         self.val = x
 #         self.next = None
 
+# passes 729/1563 tests; assumed length of l1 and l2 would be same (did not indicate either way)
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        print(l1.val, l2.val)
         num1 = num2 = ''
 
         while l1 and l2:
@@ -17,9 +17,14 @@ class Solution:
             num2 = str(l2.val) + num2
             l1, l2 = l1.next, l2.next
         total = str(int(num1) + int(num2))
-        l3 = ListNode(0)
-        l3 = l3.next
-        for i in range(len(total)-1, -1, -1):
-            l3 = ListNode(int(total[i]))
+        
+        l3 = ListNode(int(total[-1]))
+        dummy = ListNode(0)
+        dummy.next = l3
+        for i in range(len(total)-2, -1, -1):
+            l3.next = ListNode(int(total[i]))
             l3 = l3.next
-        return l3
+            
+        return dummy.next
+
+# test case: [1,8], [0]   returns [1,8]
