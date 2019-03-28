884aa8e41c4496c2e44634e64d01c087965297e1
Beverly Pham <beverly.pham@gmail.com>
2019-03-28 14:49:38 -0700

    first missing positive fastest leetcode solution added

--- leetcode/first_missing_positive.py
+++ leetcode/first_missing_positive.py
@@ -59,9 +59,23 @@ class Solution:
 
 
 # runtime 36 ms, 99%; memory 13.2 MB, 5%
+# Does not meet the constraints: Your algorithm should run in O(n) time and uses constant extra space.
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         nums = set(nums)
         for i in range(1, 100001):
             if i not in nums:
                 return i
+
+
+# not my solution (leetcode's fastest solution - 32 ms)
+# runtime 36 ms, 99%; memory 13.1 MB, 5%
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums = sorted([num for num in nums if num > 0])
+        prev = 0
+        for num in nums:
+            if num - prev > 1:
+                return prev + 1
+            prev = num
+        return prev + 1
