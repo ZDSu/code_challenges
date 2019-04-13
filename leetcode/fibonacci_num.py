358f0e27cc2577ef36f734ddc0c3b77115b259d5
Beverly Pham <beverly.pham@gmail.com>
2019-04-13 16:36:56 -0700

    another solution for fibonacci number

--- leetcode/fibonacci_num.py
+++ leetcode/fibonacci_num.py
@@ -17,3 +17,37 @@ class Solution:
             one = two
             two = res
         return res
+
+
+
+# runtime 36 ms, 79%; memory 13.1 MB, 5%
+class Solution:
+    def fib(self, N: int) -> int:
+        if N == 0:
+            return 0
+        if N == 1:
+            return 1
+
+        one = 0
+        two = 1
+        for _ in range(2, N + 1):
+            temp = one + two
+            one = two
+            two = temp
+        return two
+
+
+# supposed to be 100% solution but got same results as my solutions
+# runtime 36 ms, 79%; memory 13.3 MB, 5%
+class Solution:
+    def fib(self, N: 'int') -> 'int':
+        if N == 0: return 0
+        if N == 1: return 1
+        i = 2
+        fn0, fn1 = 0, 1
+        while(i <= N):
+            t = fn1
+            fn1 = fn1 + fn0
+            fn0 = t
+            i = i + 1
+        return fn1
