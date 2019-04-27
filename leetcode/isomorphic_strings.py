4e77fb32a3ee15c08449d16ee0fa34e187ded2ab
Beverly Pham <beverly.pham@gmail.com>
2019-04-26 17:23:14 -0700

    better solution for isomorphic strings

--- leetcode/isomorphic_strings.py
+++ leetcode/isomorphic_strings.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/isomorphic-strings/description/
 
 
+# runtime 44 ms, 84%; memory 13.4 MB, 16%
 class Solution:
     def isIsomorphic(self, s, t):
         """
@@ -16,6 +17,29 @@ class Solution:
             elif t[i] in pairs.values():
                 return False
             pairs[s[i]] = t[i]
+        return True
+
+
+# runtime 40 ms, 96%; memory 13.5 MB, 16%
+class Solution:
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        pairs = {}
+        keys = set()
+
+        for i in range(len(s)):
+            if s[i] in pairs:
+                if pairs[s[i]] != t[i]:
+                    return False
+            else:
+                if t[i] in keys:
+                    return False
+                pairs[s[i]] = t[i]
+                keys.add(t[i])
         return True
 
 
