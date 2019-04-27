6854293beccccfdf2f8f3d385600cd0eeffec3c8
Beverly Pham <beverly.pham@gmail.com>
2019-04-26 22:47:40 -0700

    add leetcode link and stats to is_unique

--- cracking/1.1_is_unique.py
+++ cracking/1.1_is_unique.py
@@ -3,9 +3,11 @@ Is Unique
 
 Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 Hints: #44, #117, #132
+
+https://leetcode.com/problems/contains-duplicate/
 """
 
-
+# 64 ms, 13.22%; 18.9 MB, 25%
 def unique(s):
     seen = set()
     for char in s:
@@ -15,12 +17,14 @@ def unique(s):
     return True
 
 
+# runtime 56 ms, 26.37%; 18.9 MB, 25%
 def unique(s):
     s_set = set(s)
     return not len(s) == len(s_set)
 
 
 # no additional data structures
+# time limit exceeded
 def unique(s):
     for i in range(len(s)):
         for j in range(len(s)):
