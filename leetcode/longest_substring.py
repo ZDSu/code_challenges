486dd55be0b3aa05466d18093554026674b17ee8
Beverly Pham <beverly.pham@gmail.com>
2019-05-13 15:55:24 -0700

    add #2 solution from user submissions, which is faster than #1; try this one again

--- leetcode/longest_substring.py
+++ leetcode/longest_substring.py
@@ -45,4 +45,23 @@ class Solution:
             longest = len(ls)
         return longest
 
+
+# runtime 48 ms, 100%; memory 13.3 MB, 5%
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        d = {}
+        start = -1
+        max = 0
+        for i in range(len(s)):
+            if s[i] in d and d[s[i]] > start:
+                start = d[s[i]]
+                d[s[i]] = i
+
+            else:
+                d[s[i]] = i
+                if i - start > max:
+                    max = i - start
+        return max
+
+
 # test cases:  "au", "dvdf"
