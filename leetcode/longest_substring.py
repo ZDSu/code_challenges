bb0391c7c9c37667c8a7eabae6fd174ace637428
Beverly Pham <beverly.pham@gmail.com>
2019-05-13 15:37:39 -0700

    longest substring wihtout repeating chars solved, but not performant at all; try again

--- leetcode/longest_substring.py
+++ leetcode/longest_substring.py
@@ -2,24 +2,26 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/solution/
 
 
-
+# runtime 640 ms, 12.5%; memory 13.1 MB, 5.5%
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         if not s:
             return 0
 
-        longest = curr = 0
-        chars = set()
+        i = longest = 0
+        chars = {}
 
-        for char in s:
-            if char in chars:
-                if curr > longest:
-                    longest = curr
-                curr = 1
-                chars = set(char)
+        while i <= len(s) - 1:
+            if s[i] not in chars:
+                chars[s[i]] = i
             else:
-                chars.add(char)
-                curr += 1
-        if curr > longest:
-            return curr
+                if len(chars) > longest:
+                    longest = len(chars)
+                temp = chars[s[i]] + 1
+                chars = {}
+                chars[s[temp]] = temp
+                i = temp
+            i += 1
+        if len(chars) > longest:
+            longest = len(chars)
         return longest
