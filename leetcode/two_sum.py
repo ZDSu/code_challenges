063e4da4b6ca7578b65a2fd8fb652ebab18017a0
Beverly Pham <beverly.pham@gmail.com>
2019-05-24 23:07:52 -0400

    add another solution for two sum

--- leetcode/two_sum.py
+++ leetcode/two_sum.py
@@ -25,9 +25,25 @@ class Solution:
         :rtype: List[int]
         """
         seen = {}
-        
+
         for i in range(len(nums)):
             need = target - nums[i]
             if need in seen:
                 return [seen[need], i]
             seen[nums[i]] = i
+
+
+# runtime 36 ms, 97%; memory 14.2 MB, 45%
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+        for i in range(len(nums)):
+            if target - nums[i] in seen:
+                return [seen[target - nums[i]], i]
+            else:
+                seen[nums[i]] = i
