523f04676c6c643fc39b765839c9978d23c5101c
Beverly Pham <beverly.pham@gmail.com>
2019-06-02 13:02:36 -0700

    another solution for linked list cycle II

--- leetcode/ll_cycle_ii.py
+++ leetcode/ll_cycle_ii.py
@@ -3,6 +3,7 @@
 
 
 # runtime: 44 ms, 100%; memory 18.4 MB, 16.15%
+# runtime: 44 ms, 73%; memory 18.6 MB, 16.09%
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
@@ -21,4 +22,19 @@ class Solution(object):
                 return head
             else:
                 seen.add(head)
+            head = head.next
+
+
+# runtime 36 ms, 97%; memory 18.7 MB, 11%
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        seen = set()
+        while head:
+            if head in seen:
+                return head
+            seen.add(head)
             head = head.next
