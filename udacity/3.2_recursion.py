f7160900fb27122310f2c89d9be16ad08a1f22a4
Beverly Pham <beverly.pham@gmail.com>
2019-06-18 22:37:36 -0700

    add 3.2 solution

--- udacity/3.2_recursion.py
+++ udacity/3.2_recursion.py
@@ -1,4 +1,5 @@
 """ Fibonacci iteratively """
+"""Javascript
 function getFib(position) {
   if (position == 0) { return 0; }
   if (position == 1) { return 1; }
@@ -12,6 +13,19 @@ function getFib(position) {
   }
   return next;
 }
+"""
+
+def get_fib(position):
+    if position == 0 or position == 1:
+        return position
+    first = 0
+    second = 1
+    next = first + second
+    for i in range(2, position):
+        first = second
+        second = next
+        next = first + second
+    return next
 
 
 """Implement a function recursively to get the desired
@@ -20,7 +34,9 @@ Your code should have the same input/output as the
 iterative code in the instructions."""
 
 def get_fib(position):
-    return -1
+    if position == 0 or position == 1:
+        return position
+    return get_fib(position - 1) + get_fib(position - 2)
 
 # Test cases
 print get_fib(9)
