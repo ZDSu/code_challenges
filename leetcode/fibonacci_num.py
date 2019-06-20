85ad69824b5d3b0c0e6bc6c6ea5482df63467930
Beverly Pham <beverly.pham@gmail.com>
2019-06-19 20:15:12 -0700

    another fibonacci

--- leetcode/fibonacci_num.py
+++ leetcode/fibonacci_num.py
@@ -74,3 +74,22 @@ class Solution:
         if N == 0 or N == 1:
             return N
         return self.fib(N - 1) + self.fib(N - 2)
+
+
+# runtime 16 ms, 90%; memory 11.8 MB, 22%
+class Solution(object):
+    def fib(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        if N == 0 or N == 1:
+            return N
+
+        first = 0
+        second = 1
+        for i in range(2, N + 1):
+            next = first + second
+            first = second
+            second = next
+        return next
