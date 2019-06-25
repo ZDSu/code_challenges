43f09675194bfc4e11cc046ec2ce6f90cb586db1
Beverly Pham <beverly.pham@gmail.com>
2019-06-25 13:22:21 -0700

    validate bst iterative solution

--- leetcode/validate_bst.py
+++ leetcode/validate_bst.py
@@ -30,7 +30,30 @@ class Solution(object):
         return traverse(root)
 
 
+# runtime 36 ms, 82%; memory 16.2 MB, 93%
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+
+        stack = [(root, float('-inf'), float('inf'))]
+        while stack:
+            node, lower, upper = stack.pop()
+            if lower >= node.val or node.val >= upper:
+                return False
+            if node.left:
+                stack.append((node.left, lower, node.val))
+            if node.right:
+                stack.append((node.right, node.val, upper))
+        return True
+
+
 # test cases:
 # [1,1]   returns False
 # [10,5,15,null,null,6,20]   returns False
 # [2,1,3]   returns True
+# []
