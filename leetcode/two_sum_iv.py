859ccc09080426422e54eaad6949c613b026c7a2
Beverly Pham <beverly.pham@gmail.com>
2019-06-25 14:40:36 -0700

    two sum iv solved

---
+++ leetcode/two_sum_iv.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+
+
+# runtime 72 ms, 79%; memory 16.1 MB, 95%
+class Solution(object):
+    def findTarget(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: bool
+        """
+        def traverse(node, seen):
+            if node:
+                if k - node.val in seen:
+                    return True
+                else:
+                    seen.add(node.val)
+                if traverse(node.left, seen) or traverse(node.right, seen):
+                    return True
+        return traverse(root, set())
