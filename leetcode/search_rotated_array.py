04a2d1dbdf9a138df901a2fab31b2025d6e67622
Beverly Pham <beverly.pham@gmail.com>
2019-06-25 22:22:37 -0700

    search in rotated sorted array solved, revisit and try again

---
+++ leetcode/search_rotated_array.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+
+# runtime 32 ms, 64%; memory 12.1 MB, 17%
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return -1
+        if len(nums) < 2:
+            if nums[0] == target:
+                return 0
+            return -1
+
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
+
+# test case: [], 5   returns -1
