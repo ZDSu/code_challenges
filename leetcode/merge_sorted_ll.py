dee811b44e0a5e6a23ec33db62c1e0be04fa618b
Beverly Pham <beverly.pham@gmail.com>
2019-06-28 14:19:11 -0700

    merge 2 sorted linked lists

--- leetcode/merge_sorted_ll.py
+++ leetcode/merge_sorted_ll.py
@@ -23,7 +23,7 @@ class Solution:
             return l1
 
         head = merged = ListNode(None)
-        
+
         while l1 and l2:
             if l1.val < l2.val:
                 merged.next = l1
@@ -38,12 +38,6 @@ class Solution:
             merged.next = l2
         return head.next
 
-
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
 
 class Solution:
     def mergeTwoLists(self, l1, l2):
@@ -62,4 +56,26 @@ class Solution:
                 l2 = l2.next
             merged = merged.next
         merged.next = l1 or l2
+        return head.next
+
+
+# runtime 20 ms, 93%; memory 11.8 MB, 50%
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        merged = head = ListNode(None)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                merged.next = l1
+                l1 = l1.next
+            else:
+                merged.next = l2
+                l2 = l2.next
+            merged = merged.next
+        if l1 or l2:
+            merged.next = l1 or l2
         return head.next
