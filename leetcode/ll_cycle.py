3254cf9004f45748c731b662f7b5fc845200e898
Beverly Pham <beverly.pham@gmail.com>
2019-06-27 23:04:12 -0700

    update linked list cycle

--- leetcode/ll_cycle.py
+++ leetcode/ll_cycle.py
@@ -2,13 +2,13 @@
 # https://leetcode.com/articles/linked-list-cycle/
 
 
-# runtime: 40 ms, 100%; memory: 16.2 MB, 92%
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
+# runtime: 40 ms, 100%; memory: 16.2 MB, 92%
 class Solution(object):
     def hasCycle(self, head):
         """
@@ -21,5 +21,21 @@ class Solution(object):
             curr1 = curr1.next
             curr2 = curr2.next.next
             if curr1 is curr2:
+                return True
+        return False
+
+
+# runtime 40 ms, 80%; memory 18.1 MB, 57%
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        a = b = head
+        while b and b.next:
+            a = a.next
+            b = b.next.next
+            if a is b:
                 return True
         return False
