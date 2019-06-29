3cd90121a2a02a2064e35ddacdd8c5ff3fec8580
Beverly Pham <beverly.pham@gmail.com>
2019-06-29 14:35:04 -0700

    add more for valid anagram

--- leetcode/valid_anagram.py
+++ leetcode/valid_anagram.py
@@ -70,6 +70,31 @@ class Solution:
         return True
 
 
+# runtime: 56 ms, 71%; memory: 13.4 MB, 38%
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s_chars = {}
+        for char in s:
+            if char in s_chars:
+                s_chars[char] += 1
+            else:
+                s_chars[char] = 1
+        for char in t:
+            if char in s_chars:
+                s_chars[char] -= 1
+                if s_chars[char] == 0:
+                    del s_chars[char]
+            else:
+                return False
+        return not s_chars
+
+
+
 # runtime 96 ms, 6%; memory 13.2 MB, 32%
 import collections
 class Solution:
@@ -88,6 +113,27 @@ class Solution:
             if str1[char] == 0:
                 del str1[char]
         return not str1
+
+
+# runtime 76 ms, 17%; memory 13.3 MB, 56%
+import collections
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s_chars = collections.Counter(s)
+        for char in t:
+            if char in s_chars:
+                s_chars[char] -= 1
+                if s_chars[char] == 0:
+                    del s_chars[char]
+            else:
+                return False
+        return not s_chars
+
 
 # test cases:
 # 'ab', 'a'
