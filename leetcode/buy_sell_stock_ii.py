260e34ddca5f8cebd8cb133ee9cb71cd6d39449d
Beverly Pham <beverly.pham@gmail.com>
2019-07-02 22:48:20 -0700

    another solution for best time to buy/sell stock ii

--- leetcode/buy_sell_stock_ii.py
+++ leetcode/buy_sell_stock_ii.py
@@ -74,3 +74,23 @@ class Solution:
             if prices[i] > prices[i - 1]:
                 profit += prices[i] - prices[i-1]
         return profit
+
+
+# based on solution 2
+# runtime 44 ms, 91%; memory 12.5 MB, 83%
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        i = profit = 0
+        while i < len(prices) - 1:
+            while i < len(prices) - 1 and prices[i] >= prices[i+1]:
+                i += 1
+            low = prices[i]
+            while i < len(prices) - 1 and prices[i] <= prices[i+1]:
+                i += 1
+            high = prices[i]
+            profit += high - low
+        return profit
