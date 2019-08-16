7861a6a36de8d31cc6a4e49e829df8d7f78257f5
Beverly Pham <beverly.pham@gmail.com>
2019-08-16 15:09:48 -0700

    add solution answer for top k frequent words

--- leetcode/top_k_freq_words.py
+++ leetcode/top_k_freq_words.py
@@ -18,3 +18,19 @@ class Solution(object):
         for _ in range(k):
             res.append(heapq.heappop(maxheap)[1])
         return res
+
+
+# from solution
+# runtime 56 ms, 10%; memory 12 MB, 12%
+import heapq
+class Solution(object):
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+        count = collections.Counter(words)
+        maxheap = [(-f, w) for w, f in count.items()]
+        heapq.heapify(maxheap)
+        return [heapq.heappop(maxheap)[1] for _ in range(k)]
