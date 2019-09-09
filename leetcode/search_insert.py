4736e6f90ca5103171ff0e189d98fc7cb0d322ff
Beverly Pham <beverly.pham@gmail.com>
2019-09-09 13:59:19 -0700

    add binary search solution for search insert position

--- leetcode/search_insert.py
+++ leetcode/search_insert.py
@@ -27,6 +27,33 @@ class Solution:
                 return i
         return len(nums)
 
+
+# runtime 32 ms, 86%; memory 12.1 MB, 100%
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        if nums[l] < target:
+            return l + 1
+        else:
+            return l
+
+
 # test cases:
 # [1], 1  returns 0
 # [1,3], 1  returns 0
+# [1,3], 0  returns 0
