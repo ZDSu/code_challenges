f87d8c964e1e36bec47188f52965ed6f4bb05459
Beverly Pham <beverly.pham@gmail.com>
2019-09-09 21:50:09 -0700

    add to find minimum in rotated sorted array

--- leetcode/min_rotated_array.py
+++ leetcode/min_rotated_array.py
@@ -3,6 +3,7 @@
 
 
 # runtime 28 ms, 76%; memory 12 MB, 45%
+# runtime 28 ms, 57%; memory 12 MB, 60%
 class Solution(object):
     def findMin(self, nums):
         """
@@ -19,6 +20,7 @@ class Solution(object):
 
 
 # runtime 16 ms, 100%; memory 12.1 MB, 13%
+# runtime 24 ms, 84%; memory 12.1 MB, 20%
 class Solution(object):
     def findMin(self, nums):
         """
@@ -42,6 +44,30 @@ class Solution(object):
                 right = mid - 1
 
 
-# test case:
+# runtime 24 ms, 84%; memory 12 MB, 60%
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) < 2 or nums[0] < nums[-1]:
+            return nums[0]
+
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid - 1] > nums[mid]:
+                return nums[mid]
+            if nums[mid] > nums[mid + 1]:
+                return nums[mid + 1]
+            if nums[mid] > nums[0]:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+
+# test cases:
 # [1,2]  returns 1
 # [4,5,1,2,3]  returns 1
+# [2,1]  returns 1
