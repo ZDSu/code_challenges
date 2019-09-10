f0d8ab25abd93cffdf6818eaf002b180f3e84ee6
Beverly Pham <beverly.pham@gmail.com>
2019-09-09 21:52:23 -0700

    find first and last position in sorted array solved

--- leetcode/first_last_sorted_arr.py
+++ leetcode/first_last_sorted_arr.py
@@ -48,3 +48,53 @@ class Solution(object):
         if nums[l] == target:
             return l
         return -1
+
+
+# hackerrank/pathrise solution
+# runtime 72 ms, 53%; memory 13 MB, 79%
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if not nums:
+            return [-1, -1]
+
+        l, r = 0, len(nums) - 1
+
+        while l + 1 < r:
+            mid = (l + r) // 2
+            if nums[mid] >= target:
+                r = mid
+            else:
+                l = mid
+
+        if nums[l] == target:
+            left = l
+        elif nums[r] == target:
+            left = r
+        else:
+            left = -1
+
+        l, r = 0, len(nums) - 1
+
+        while l + 1 < r:
+            mid = (l + r) // 2
+            if nums[mid] <= target:
+                l = mid
+            else:
+                r = mid
+
+        if nums[r] == target:
+            right = r
+        elif nums[l] == target:
+            right = l
+        else:
+            right = -1
+
+        return [left, right]
+
+
+# test case: [], 0
