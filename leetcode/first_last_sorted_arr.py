a8ee06cf9e28e327faa902969d5763ed802fe4f8
Beverly Pham <beverly.pham@gmail.com>
2019-09-11 11:54:31 -0700

    add pathrise solution for find range in a sorted array

--- leetcode/first_last_sorted_arr.py
+++ leetcode/first_last_sorted_arr.py
@@ -50,7 +50,7 @@ class Solution(object):
         return -1
 
 
-# hackerrank/pathrise solution
+# pathrise assignment hackerrank solution
 # runtime 72 ms, 53%; memory 13 MB, 79%
 class Solution(object):
     def searchRange(self, nums, target):
@@ -95,6 +95,42 @@ class Solution(object):
             right = -1
 
         return [left, right]
+
+
+# Pathrise solution [Find Range in a Sorted Array] (Erik/Pair Programming)
+# runtime: 68ms, 79%; memory 13.1 MB, 53%
+def find_bound(array, target, look_left):
+    low = 0
+    high = len(array) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if array[mid] < target:
+            low = mid + 1
+        elif array[mid] > target:
+            high = mid - 1
+        else:
+            if look_left:
+                if mid == 0 or array[mid-1] != target:
+                    return mid
+                else:
+                    high = mid - 1
+            else: # look_right
+                if mid == len(array)-1 or array[mid+1] != target:
+                    return mid
+                else:
+                    low = mid + 1
+    return -1
+
+class Solution(object):
+    def searchRange(self, array, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        start_idx = find_bound(array, target, True)
+        end_idx = find_bound(array, target, False)
+        return (start_idx,end_idx)
 
 
 # test case: [], 0
