507f0e03d89af47b9b6f052bbfbb524117b8aa17
Beverly Pham <beverly.pham@gmail.com>
2019-09-12 22:06:23 -0700

    add another solution and test cases for missing number

--- leetcode/missing_number.py
+++ leetcode/missing_number.py
@@ -2,6 +2,7 @@
 # https://leetcode.com/articles/missing-number/
 
 
+# runtime 128 ms, 22%; memory 12.8 MB, 37%
 class Solution:
     def missingNumber(self, nums):
         """
@@ -14,3 +15,22 @@ class Solution:
                 return each - 1
             current += 1
         return each + 1
+
+
+# runtime 116 ms, 60%; memory 13.2 MB, 7%
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        numbers = set(nums)
+        for i in range(len(nums) + 1):
+            if i not in numbers:
+                return i
+
+
+# test cases:
+# [0]  returns 1
+# [1]  returns 0
+# [0,1]  returns 2
