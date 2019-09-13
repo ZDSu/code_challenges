3062936aa420f482dfa0fde72c7924bb41c273fd
Beverly Pham <beverly.pham@gmail.com>
2019-09-12 18:33:55 -0700

    add recursive solution for min depth of binary tree

--- leetcode/bt_min_depth.py
+++ leetcode/bt_min_depth.py
@@ -35,6 +35,42 @@ class Solution(object):
         return minimum
 
 
+# after Pathrise lesson
+# runtime 32 ms, 73%; memory 14.6 MB, 67%
+def getHeight(root):
+    if not root:
+        return 0
+
+    if not root.left:
+        right = getHeight(root.right)
+        return right + 1
+    if not root.right:
+        left = getHeight(root.left)
+        return left + 1
+
+    left = getHeight(root.left)
+    right = getHeight(root.right)
+    return min(left, right) + 1
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        left = getHeight(root.left)
+        right = getHeight(root.right)
+
+        if not left or not right:
+            return (left or right) + 1
+
+        return min(left, right) + 1
+
 # test cases:
 # [0]  returns 1
 # [1,2]  returns 2
+# [1,null,2]  returns 2
+# [-9,-3,2,null,4,4,0,-6,null,-5]  returns 3
