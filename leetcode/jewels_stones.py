3419ee9a005d773c65ffa5396191d4c1c06480ea
Beverly Pham <beverly.pham@gmail.com>
2019-09-15 10:36:42 -0700

    jewels and stones two-liner condensed code from solution

--- leetcode/jewels_stones.py
+++ leetcode/jewels_stones.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/jewels-and-stones/
 # https://leetcode.com/problems/jewels-and-stones/solution/
 
+
 # runtime 40 ms, 43%; memory 13.9 MB, 5%
 # runtime 32 ms, 93%; memory 13.8 MB, 5%
 class Solution:
@@ -13,3 +14,11 @@ class Solution:
                 res += 1
 
         return res
+
+
+# runtime 40 ms, 43%; memory 14 MB, 5%
+# condensed code from solution
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        jewels = set(J)
+        return sum(s in jewels for s in S)
