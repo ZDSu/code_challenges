831bebd915fb5871047de1783d3f371810086e83
Beverly Pham <beverly.pham@gmail.com>
2019-09-15 14:09:29 -0700

    add solution descriptino for find anagram mapping (premium)

--- leetcode/find_anagram_mappings.py
+++ leetcode/find_anagram_mappings.py
@@ -37,3 +37,22 @@ class Solution:
     def anagramMappings(self, A: List[int], B: List[int]) -> List[int]:
         D = {x: i for i, x in enumerate(B)}
         return [D[x] for x in A]
+
+
+"""
+Solution
+Approach #1: Hash Table [Accepted]
+
+Intuition
+Take the example A = [12, 28, 46], B = [46, 12, 28]. We want to know where the 12 occurs in B, say at position 1; then where the 28 occurs in B, which is position 2; then where the 46 occurs in B, which is position 0.
+
+If we had a dictionary (hash table) D = {46: 0, 12: 1, 28: 2}, then this question could be handled easily.
+
+Algorithm
+Create the hash table D as described above. Then, the answer is a list of D[A[i]] for i = 0, 1, ....
+[code is above]
+
+Complexity Analysis
+- Time Complexity: O(N), where N is the length of A.
+- Space Complexity: O(N).
+"""
