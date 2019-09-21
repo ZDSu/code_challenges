0d473712d24949a117d6676275a8c767d364dd94
Beverly Pham <beverly.pham@gmail.com>
2019-09-21 11:37:26 -0700

    another solution for subset using template

--- leetcode/subsets.py
+++ leetcode/subsets.py
@@ -18,3 +18,18 @@ class Solution:
         self.dfs(nums, temp, index + 1)
         temp.pop()
         self.dfs(nums, temp, index + 1)
+
+
+# runtime 40 ms, 75%; memory 13.8 MB, 6%
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        self.dfs(nums, 0, [])
+        return self.res
+
+    def dfs(self, nums, start_index, temp):
+        self.res.append(temp[:])
+        for i in range(start_index, len(nums)):
+            temp.append(nums[i])
+            self.dfs(nums, i + 1, temp)
+            temp.pop()
