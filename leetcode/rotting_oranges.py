b275859fc5c780fe8767d357f6f61fa329781f5b
Beverly Pham <beverly.pham@gmail.com>
2019-09-26 19:52:11 -0700

    another solution for rotting oranges, cleaned it up a little

--- leetcode/rotting_oranges.py
+++ leetcode/rotting_oranges.py
@@ -54,4 +54,49 @@ class Solution:
         return res
 
 
+# runtime 56 ms, 92%; memory 13.8 MB, 17%
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        queue = []
+        res = oranges = 0
+        lx = len(grid)-1
+        ly = len(grid[0])-1
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 2:
+                    queue.append([i, j])
+                if grid[i][j] == 1:
+                    oranges += 1
+
+        if not oranges:
+            return 0
+
+        while queue:
+            for i in range(len(queue)):
+                x,y = queue.pop(0)
+                if grid[x][y] == 2:
+                    if x < lx and grid[x + 1][y] == 1:
+                        grid[x + 1][y] = 2
+                        queue.append([x + 1, y])
+                        oranges -= 1
+                    if y < ly and grid[x][y + 1] == 1:
+                        grid[x][y + 1] = 2
+                        queue.append([x, y + 1])
+                        oranges -= 1
+                    if 0 < x and grid[x - 1][y] == 1:
+                        grid[x - 1][y] = 2
+                        queue.append([x - 1, y])
+                        oranges -= 1
+                    if 0 < y and grid[x][y - 1] == 1:
+                        grid[x][y - 1] = 2
+                        queue.append([x, y - 1])
+                        oranges -= 1
+            res += 1
+
+        if oranges:
+            return -1
+        return res - 1
+
+
 # test case: [[1,2]]  returns 1
