0cd3d3a8e9481be2f5f1a6b3f69e014cce211e0e
Beverly Pham <beverly.pham@gmail.com>
2019-09-29 22:56:31 -0700

    add solutions for meeting rooms

--- leetcode/meeting_rooms.py
+++ leetcode/meeting_rooms.py
@@ -13,6 +13,71 @@ Input: [[7,10],[2,4]]
 Output: true
 """
 
+# runtime 100 ms, 12%; memory 17.3 MB, 8%
+import heapq
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        pass
+        if not intervals:
+            return True
+
+        intervals.sort(key = lambda x : x[0])
+        pq = []
+        heapq.heappush(pq, intervals[0][1])
+
+        for interval in intervals[1:]:
+            if interval[0] < pq[0]:
+                return False
+            heapq.heappop(pq)
+            heapq.heappush(pq, interval[1])
+        return True
+
+
+# runtime 96 ms, 19%; memory 17.1 MB, 8%
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        l1 = []
+        for i in intervals:
+            for j in i:
+                l1.append(j)
+        l1.sort()
+        l2 = []
+        intervals.sort()
+        for i in intervals:
+            for j in i:
+                l2.append(j)
+        return l1 == l2
+
+
+# runtime 92 ms, 48%; memory 17.3 MB, 8%
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if intervals == []:
+            return True
+
+        intervals = sorted(intervals, key = lambda x:x[0])
+        prev = intervals[0]
+
+        for lst in intervals[1:]:
+            if lst[0] < prev[1]:
+                return False
+            prev = lst
+        return True
+
+
+# runtime 92 ms, 48%; memory 17.3 MB, 8%
+# import heapq
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if not intervals:
+            return True
+
+        heapq.heapify(intervals)
+        end = heapq.heappop(intervals)[1]
+
+        while len(intervals) > 0:
+            meeting = heapq.heappop(intervals)
+            if meeting[0] < end:
+                return False
+            end = meeting[1]
+
+        return True
