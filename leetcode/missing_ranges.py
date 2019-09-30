507a13e4db308276cecd96bf343ef4be3da1f9a3
Beverly Pham <beverly.pham@gmail.com>
2019-09-29 17:02:25 -0700

    add problem statement for missing ranges, need to add solution

--- leetcode/missing_ranges.py
+++ leetcode/missing_ranges.py
@@ -9,3 +9,7 @@ Input: nums = [0, 1, 3, 50, 75], lower = 0 and upper = 99,
 Output: ["2", "4->49", "51->74", "76->99"]
 
 """
+
+class Solution:
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
+        pass
