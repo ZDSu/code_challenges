9d9bcaf235ab7bdcd8e86927a012339816a63e4f
Beverly Pham <beverly.pham@gmail.com>
2019-10-01 20:00:35 -0700

    add task scheduler and solution

--- leetcode/task_scheduler.py
+++ leetcode/task_scheduler.py
@@ -1,4 +1,9 @@
 # https://leetcode.com/problems/task-scheduler/
 # https://leetcode.com/problems/task-scheduler/solution/
 
-# also see p@thrise dr0pbox for solution
+# also see p@thrise dr0pbox prog_ses for their solution
+
+"""
+Task Scheduler (Medium) (Premium)
+
+"""
