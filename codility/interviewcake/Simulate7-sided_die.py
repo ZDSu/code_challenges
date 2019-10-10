b5489d12e5108307a9ee5ab583e581bfee318164
Beverly Pham <beverly.pham@gmail.com>
2019-10-10 11:32:55 -0700

    add interviewcake simulate 7 sided diegpom

---
+++ codility/interviewcake/Simulate7-sided_die.py
@@ -0,0 +1,17 @@
+import random
+
+
+def rand5():
+    return random.randint(1, 5)
+
+
+def rand7():
+
+    # Implement rand7() using rand5()
+
+
+    return 0
+
+
+print('Rolling 7-sided die...')
+print(rand7())
