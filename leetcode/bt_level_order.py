0b83ba2a903d9e85c39d7463cb5a2700d8fe6bc0
Beverly Pham <beverly.pham@gmail.com>
2019-10-11 09:03:49 -0700

    add new solution article for binary tree level order

--- leetcode/bt_level_order.py
+++ leetcode/bt_level_order.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/binary-tree-level-order-traversal/
 # https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/931/
+# https://leetcode.com/articles/binary-tree-level-order-traversal
 
 
 # runtime 28 ms, 82%; memory 11.5 MB, 46%
@@ -41,3 +42,41 @@ class Solution:
 # test cases:
 # []  returns []
 # [1,2,3,4,5] returns [[1],[2,3],[4,5]]
+
+
+# Approach 2 solution code
+from collections import deque
+class Solution:
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        levels = []
+        if not root:
+            return levels
+
+        level = 0
+        queue = deque([root,])
+        while queue:
+            # start the current level
+            levels.append([])
+            # number of elements in the current level
+            level_length = len(queue)
+
+            for i in range(level_length):
+                node = queue.popleft()
+                # fulfill the current level
+                levels[level].append(node.val)
+
+                # add child nodes of the current level
+                # in the queue for the next level
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            # go to next level
+            level += 1
+
+        return levels
