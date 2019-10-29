57036d01a5becd03e507f92ef86f8e9da3c6d019
Beverly Pham <beverly.pham@gmail.com>
2019-10-28 18:03:41 -0700

    add word abbreviation and solution article

---
+++ leetcode/word_abbr.py
@@ -0,0 +1,93 @@
+# https://leetcode.com/problems/word-abbreviation (Premium)
+# https://leetcode.com/articles/word-abbreviation/
+
+
+"""
+Word Abbreviation (Hard)
+Given an array of n distinct non-empty strings, you need to generate minimal possible abbreviations for every word following rules below.
+
+1. Begin with the first character and then the number of characters abbreviated, which followed by the last character.
+2. If there are any conflict, that is more than one words share the same abbreviation, a longer prefix is used instead of only the first character until making the map from word to abbreviation become unique. In other words, a final abbreviation cannot map to more than one original words.
+3. If the abbreviation doesn't make the word shorter, then keep it as original.
+
+Example:
+
+Input: ["like", "god", "internal", "me", "internet", "interval", "intension", "face", "intrusion"]
+Output: ["l2e","god","internal","me","i6t","interval","inte4n","f2e","intr4n"]
+
+Note:
+1. Both n and the length of each word will not exceed 400.
+2. The length of each word is greater than 1.
+3. The words consist of lowercase English letters only.
+4. The return answers should be in the same order as the original array.
+"""
+
+class Solution:
+    def wordsAbbreviation(self, dict: List[str]) -> List[str]:
+        pass
+
+
+
+
+# Approach 2 solution code
+class Solution(object):
+    def wordsAbbreviation(self, words):
+        def longest_common_prefix(a, b):
+            i = 0
+            while i < len(a) and i < len(b) and a[i] == b[i]:
+                i += 1
+            return i
+
+        ans = [None for _ in words]
+
+        groups = collections.defaultdict(list)
+        for index, word in enumerate(words):
+            groups[len(word), word[0], word[-1]].append((word, index))
+
+        for (size, first, last), enum_words in groups.iteritems():
+            enum_words.sort()
+            lcp = [0] * len(enum_words)
+            for i, (word, _) in enumerate(enum_words):
+                if i:
+                    word2 = enum_words[i-1][0]
+                    lcp[i] = longest_common_prefix(word, word2)
+                    lcp[i-1] = max(lcp[i-1], lcp[i])
+
+            for (word, index), p in zip(enum_words, lcp):
+                delta = size - 2 - p
+                if delta <= 1:
+                    ans[index] = word
+                else:
+                    ans[index] = word[:p+1] + str(delta) + last
+
+        return ans
+
+
+# Approach 3 solution code
+class Solution(object):
+    def wordsAbbreviation(self, words):
+        groups = collections.defaultdict(list)
+        for index, word in enumerate(words):
+            groups[len(word), word[0], word[-1]].append((word, index))
+
+        ans = [None] * len(words)
+        Trie = lambda: collections.defaultdict(Trie)
+        COUNT = False
+        for group in groups.itervalues():
+            trie = Trie()
+            for word, _ in group:
+                cur = trie
+                for letter in word[1:]:
+                    cur[COUNT] = cur.get(COUNT, 0) + 1
+                    cur = cur[letter]
+
+            for word, index in group:
+                cur = trie
+                for i, letter in enumerate(word[1:], 1):
+                    if cur[COUNT] == 1: break
+                    cur = cur[letter]
+                if len(word) - i - 1 > 1:
+                    ans[index] = word[:i] + str(len(word) - i - 1) + word[-1]
+                else:
+                    ans[index] = word
+        return ans
