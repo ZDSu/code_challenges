2e4799c9a891195b03691a33706aa818ede18a3a
Beverly Pham <beverly.pham@gmail.com>
2019-11-16 19:53:53 -0800

    add convert binary search tree to doubly linked list and solution

---
+++ leetcode/convert_bst_dll.py
@@ -0,0 +1,78 @@
+# https://leetcode.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list/ (Premium)
+# https://leetcode.com/articles/convert-binary-search-tree-to-sorted-doubly-linked/#
+
+"""
+Convert Binary Search Tree to Sorted Doubly Linked List (Medium)
+Convert a BST to a sorted circular doubly-linked list in-place. Think of the left and right pointers as synonymous to the previous and next pointers in a doubly-linked list.
+
+Let's take the following BST as an example, it may help you understand the problem better:
+
+        4
+      /   \
+    2       5
+  /   \
+1       3
+
+We want to transform this BST into a circular doubly linked list. Each node in a doubly linked list has a predecessor and successor. For a circular doubly linked list, the predecessor of the first element is the last element, and the successor of the last element is the first element.
+
+The figure below shows the circular doubly linked list for the BST above. The "head" symbol means the node it points to is the smallest element of the linked list.
+<image>
+
+Specifically, we want to do the transformation in place. After the transformation, the left pointer of the tree node should point to its predecessor, and the right pointer should point to its successor. We should return the pointer to the first element of the linked list.
+
+The figure below shows the transformed BST. The solid line indicates the successor relationship, while the dashed line means the predecessor relationship.
+<image>
+"""
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+class Solution:
+    def treeToDoublyList(self, root: 'Node') -> 'Node':
+        pass
+
+
+
+
+# solution code
+class Solution:
+    def treeToDoublyList(self, root: 'Node') -> 'Node':
+        def helper(node):
+            """
+            Performs standard inorder traversal:
+            left -> node -> right
+            and links all nodes into DLL
+            """
+            nonlocal last, first
+            if node:
+                # left
+                helper(node.left)
+                # node
+                if last:
+                    # link the previous node (last)
+                    # with the current one (node)
+                    last.right = node
+                    node.left = last
+                else:
+                    # keep the smallest node
+                    # to close DLL later on
+                    first = node
+                last = node
+                # right
+                helper(node.right)
+
+        if not root:
+            return None
+
+        # the smallest (first) and the largest (last) nodes
+        first, last = None, None
+        helper(root)
+        # close DLL
+        last.right = first
+        first.left = last
+        return first
