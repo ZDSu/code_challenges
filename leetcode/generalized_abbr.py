6f5184a18fe034b5627c74abb6c0b39ed043fb47
Beverly Pham <beverly.pham@gmail.com>
2019-11-18 21:23:54 -0800

    add generalized abbreviation and solution

---
+++ leetcode/generalized_abbr.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/generalized-abbreviation/ (Premium)
+# https://leetcode.com/articles/generalized-abbreviation/
+
+
+"""
+Generalized Abbreviation (Medium)
+Write a function to generate the generalized abbreviations of a word.
+
+Note: The order of the output does not matter.
+
+Example:
+Input: "word"
+Output:
+["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
+"""
+
+class Solution:
+    def generateAbbreviations(self, word: str) -> List[str]:
+        pass
