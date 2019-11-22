02aa3b590f804f6e8735becb7908ac63e0272724
Beverly Pham <beverly.pham@gmail.com>
2019-11-22 14:38:08 -0800

    add two sum iii

---
+++ leetcode/two_sum_iii.py
@@ -0,0 +1,46 @@
+# https://leetcode.com/problems/two-sum-iii-data-structure-design/ (Premium)
+
+
+"""
+Two Sum III - Data structure design
+Design and implement a TwoSum class. It should support the following operations: add and find.
+
+add - Add the number to an internal data structure.
+find - Find if there exists any pair of numbers which sum is equal to the value.
+
+Example 1:
+add(1); add(3); add(5);
+find(4) -> true
+find(7) -> false
+
+Example 2:
+add(3); add(1); add(2);
+find(3) -> true
+find(6) -> false
+"""
+
+class TwoSum:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+
+
+    def add(self, number: int) -> None:
+        """
+        Add the number to an internal data structure..
+        """
+
+
+    def find(self, value: int) -> bool:
+        """
+        Find if there exists any pair of numbers which sum is equal to the value.
+        """
+
+
+
+# Your TwoSum object will be instantiated and called as such:
+# obj = TwoSum()
+# obj.add(number)
+# param_2 = obj.find(value)
