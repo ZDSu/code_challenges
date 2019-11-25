310a914e9ae327d968887bf3a4fe793370eb5685
Beverly Pham <beverly.pham@gmail.com>
2019-11-24 18:44:14 -0800

    add score paren

---
+++ leetcode/score_parens.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/score-of-parentheses/
+# https://leetcode.com/problems/score-of-parentheses/solution/
+
+
+class Solution:
+    def scoreOfParentheses(self, S: str) -> int:
+        pass
