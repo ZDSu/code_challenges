8708a647744d216af57a3fb80f08aa6c08474c67
Beverly Pham <beverly.pham@gmail.com>
2020-01-12 19:33:12 -0800

    add interv1ewcake two egg problem and editor code

---
+++ codility/interviewcake/Two_Egg_Problem.py
@@ -0,0 +1,12 @@
+# (this is actually just the default editor code--doesn't apply to problem)
+
+def my_function(arg):
+
+    # Write the body of your function here
+
+
+    return 'running with %s' % (arg)
+
+# Run your function through some test cases here
+# Remember: debugging is half the battle!
+print(my_function('test input'))
